615e12137d3b2126136a0839e310606e6ea0a3c2
Phi Dang <phidang@tgm.vn>
2022-04-01 09:34:14 +0700

    BEIN-4987: get newfeed

--- src/listeners/comment/comment.listener.ts
+++ src/listeners/comment/comment.listener.ts
@@ -6,16 +6,36 @@ import {
 import { On } from '../../common/decorators';
 import { Injectable, Logger } from '@nestjs/common';
 import { NotificationService } from '../../notification';
+import { ElasticsearchHelper } from '../../common/helpers';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 @Injectable()
 export class CommentListener {
   private _logger = new Logger(CommentListener.name);
-  public constructor(private _notificationService: NotificationService) {}
+  public constructor(
+    private _notificationService: NotificationService,
+    private _elasticsearchService: ElasticsearchService
+  ) {}
 
   @On(CommentHasBeenCreatedEvent)
   public async onCommentHasBeenCreated(event: CommentHasBeenCreatedEvent): Promise<void> {
     this._logger.debug(`[CommentHasBeenCreatedEvent]: ${JSON.stringify(event)}`);
     this._notificationService.publishCommentNotification<any>(null);
+
+    const index = ElasticsearchHelper.INDEX.POST;
+    const { post } = event.payload;
+    try {
+      const dataUpdate = {
+        commentsCount: post.commentsCount + 1,
+      };
+      await this._elasticsearchService.update({
+        index,
+        id: `${post.id}`,
+        body: { doc: dataUpdate },
+      });
+    } catch (error) {
+      this._logger.error(error, error?.stack);
+    }
   }
 
   @On(CommentHasBeenUpdatedEvent)
@@ -27,6 +47,20 @@ export class CommentListener {
   @On(CommentHasBeenDeletedEvent)
   public async onCommentHasBeenDeleted(event: CommentHasBeenDeletedEvent): Promise<void> {
     this._logger.log(event);
+    const index = ElasticsearchHelper.INDEX.POST;
+    const { post } = event.payload;
+    try {
+      const dataUpdate = {
+        commentsCount: post.commentsCount - 1,
+      };
+      await this._elasticsearchService.update({
+        index,
+        id: `${post.id}`,
+        body: { doc: dataUpdate },
+      });
+    } catch (error) {
+      this._logger.error(error, error?.stack);
+    }
     // this._notificationService.publishCommentNotification<any>(null);
   }
 }
