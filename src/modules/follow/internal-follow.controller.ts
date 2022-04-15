8ece33ada83218d10b34b563abdf2ca676833ac6
The <thevan@tgm.vn>
2022-04-15 17:55:04 +0700

    BEIN-4987 add internal follow controller

--- src/modules/follow/internal-follow.controller.ts
+++ src/modules/follow/internal-follow.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Logger } from '@nestjs/common';
 import { FollowService } from './follow.service';
-import { MessageBody } from '@nestjs/websockets';
-import { EventPattern } from '@nestjs/microservices';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { EVENTS } from '../../common/constants';
 import { CreateFollowDto, UnfollowDto } from './dto/requests';
 
@@ -11,13 +10,13 @@ export class InternalFollowController {
   public constructor(private _followService: FollowService) {}
 
   @EventPattern(EVENTS.BEIN_GROUP.USERS_FOLLOW_GROUPS)
-  public async follow(@MessageBody('value') createFollowDto: CreateFollowDto): Promise<void> {
+  public async follow(@Payload('value') createFollowDto: CreateFollowDto): Promise<void> {
     this._logger.debug(`[follow]: ${JSON.stringify(createFollowDto)}`);
     await this._followService.follow(createFollowDto);
   }
 
   @EventPattern(EVENTS.BEIN_GROUP.USERS_UNFOLLOW_GROUP)
-  public async unfollow(@MessageBody('value') unfollowDto: UnfollowDto): Promise<void> {
+  public async unfollow(@Payload('value') unfollowDto: UnfollowDto): Promise<void> {
     this._logger.debug(`[unfollow]: ${JSON.stringify(unfollowDto)}`);
     await this._followService.unfollow(unfollowDto);
   }
