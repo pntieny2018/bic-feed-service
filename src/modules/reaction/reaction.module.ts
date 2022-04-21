43715ccb59396e8a671d64b6a419232238a4a746
The <thevan@tgm.vn>
2022-04-21 11:34:03 +0700

    BEIN-5466 handle dynamic queue

--- src/modules/reaction/reaction.module.ts
+++ src/modules/reaction/reaction.module.ts
@@ -4,12 +4,27 @@ import { NotificationModule } from '../../notification';
 import { GroupModule } from '../../shared/group';
 import { UserModule } from '../../shared/user';
 import { ReactionController } from './reaction.controller';
-import { CommonReactionService, CreateReactionService, DeleteReactionService } from './services';
+import {
+  CommonReactionService,
+  CreateOrDeleteReactionService,
+  CreateReactionService,
+  DeleteReactionService,
+} from './services';
 
 @Module({
   imports: [DatabaseModule, UserModule, GroupModule, NotificationModule],
   controllers: [ReactionController],
-  providers: [CreateReactionService, DeleteReactionService, CommonReactionService],
-  exports: [CommonReactionService, CreateReactionService, DeleteReactionService],
+  providers: [
+    CreateReactionService,
+    DeleteReactionService,
+    CommonReactionService,
+    CreateOrDeleteReactionService,
+  ],
+  exports: [
+    CommonReactionService,
+    CreateReactionService,
+    DeleteReactionService,
+    CreateOrDeleteReactionService,
+  ],
 })
 export class ReactionModule {}
