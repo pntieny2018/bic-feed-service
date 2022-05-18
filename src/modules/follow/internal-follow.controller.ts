3f32229e1b41fd28c985a62e979a07de58caf69a
Vu Quang Le <levuquang2k1@gmail.com>
2022-05-18 13:59:40 +0700

    BEIN-5892: refactor code

--- src/modules/follow/internal-follow.controller.ts
+++ src/modules/follow/internal-follow.controller.ts
@@ -1,29 +1,21 @@
-import { Body, Controller, Delete, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { EventPattern, Payload } from '@nestjs/microservices';
-import { APP_VERSION, KAFKA_TOPIC } from '../../common/constants';
+import { KAFKA_TOPIC } from '../../common/constants';
 import { CreateFollowDto, UnfollowDto } from './dto/requests';
-import { ApiTags } from '@nestjs/swagger';
 
-// @Controller()
-@ApiTags('Follow')
-@Controller({
-  version: APP_VERSION,
-  path: 'follows',
-})
+@Controller()
 export class InternalFollowController {
   private _logger = new Logger(InternalFollowController.name);
   public constructor(private _followService: FollowService) {}
 
-  // @EventPattern(EVENTS.BEIN_GROUP.USERS_FOLLOW_GROUPS)
+  @EventPattern(KAFKA_TOPIC.BEIN_GROUP.USERS_FOLLOW_GROUPS)
   public async follow(@Payload('value') createFollowDto: CreateFollowDto): Promise<void> {
     this._logger.debug(`[follow]: ${JSON.stringify(createFollowDto)}`);
     await this._followService.follow(createFollowDto);
   }
 
-  // @EventPattern(EVENTS.BEIN_GROUP.USERS_UNFOLLOW_GROUP)
-  // public async unfollow(@Payload('value') unfollowDto: UnfollowDto): Promise<void> {
-  @Delete('/')
+  @EventPattern(KAFKA_TOPIC.BEIN_GROUP.USERS_UNFOLLOW_GROUP)
   public async unfollow(@Body() unfollowDto: UnfollowDto): Promise<void> {
     this._logger.debug(`[unfollow]: ${JSON.stringify(unfollowDto)}`);
     await this._followService.unfollow(unfollowDto);
