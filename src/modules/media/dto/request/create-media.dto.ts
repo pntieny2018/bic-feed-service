0778e7955ed5bea3f951207b9bf3da26d2e99ba1
The <thevan@tgm.vn>
2022-05-27 17:09:49 +0700

    BEIN-5792 update for create media

--- src/modules/media/dto/request/create-media.dto.ts
+++ src/modules/media/dto/request/create-media.dto.ts
@@ -1,22 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 import { UploadType } from '../../../upload/dto/requests/upload.dto';
+import { Expose } from 'class-transformer';
 
 export class CreateMediaDto {
   @ApiProperty({
     description: 'uploadId get from upload service',
     type: String,
     example: '7663faa6-d008-404f-8e43-ce888b2b9aa8',
+    name: 'upload_id',
   })
   @IsNotEmpty()
+  @Expose({
+    name: 'upload_id',
+  })
   public uploadId: string;
 
   @ApiProperty({
     description: 'Content of post',
     enum: UploadType,
+    name: 'upload_type',
   })
   @IsNotEmpty()
   @IsEnum(UploadType)
+  @Expose({
+    name: 'upload_type',
+  })
   public uploadType: UploadType;
 
   @ApiProperty({
@@ -48,8 +57,12 @@ export class CreateMediaDto {
     description: 'mimeType',
     type: String,
     required: false,
+    name: 'mime_type',
   })
   @IsOptional()
+  @Expose({
+    name: 'mime_type',
+  })
   public mimeType?: string;
 
   @ApiProperty({
