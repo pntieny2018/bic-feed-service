631344606fc3fd88adf3ad6c1d85d0e85ac89fce
Phi Dang <phidang@tgm.vn>
2022-05-30 18:15:59 +0700

    update article module

--- src/modules/article/article.service.ts
+++ src/modules/article/article.service.ts
@@ -1,22 +1,14 @@
-import { HTTP_STATUS_ID, KAFKA_PRODUCER, MentionableType } from '../../common/constants';
+import { HTTP_STATUS_ID } from '../../common/constants';
 import { InjectConnection, InjectModel } from '@nestjs/sequelize';
-import { IPost, PostModel } from '../../database/models/post.model';
-import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
+import { PostModel } from '../../database/models/post.model';
+import { Injectable, Logger } from '@nestjs/common';
 import { UserDto } from '../auth';
 import { MediaService } from '../media';
 import { MentionService } from '../mention';
 import { CommentService } from '../comment';
 import { AuthorityService } from '../authority';
-import { UserService } from '../../shared/user';
 import { Sequelize } from 'sequelize-typescript';
-import { GroupService } from '../../shared/group';
-import { EntityType } from '../media/media.constants';
-import { LogicException } from '../../common/exceptions';
-import { MediaModel, MediaStatus } from '../../database/models/media.model';
-import { MentionModel } from '../../database/models/mention.model';
-import { PostGroupModel } from '../../database/models/post-group.model';
-import { PostReactionModel } from '../../database/models/post-reaction.model';
-import { ArrayHelper, ExceptionHelper } from '../../common/helpers';
+import { ExceptionHelper } from '../../common/helpers';
 import { ReactionService } from '../reaction';
 import { SentryService } from '../../../libs/sentry/src';
 import { CreateArticleDto } from './dto/requests/create-article.dto';
@@ -64,74 +56,7 @@ export class ArticleService {
     postId: string,
     user: UserDto,
     getArticleDto?: GetArticleDto
-  ): Promise<ArticleResponseDto> {
-    const post = await this.postModel.findOne({
-      attributes: {
-        exclude: ['updatedBy'],
-        include: [PostModel.loadMarkReadPost(user.id)],
-      },
-      where: { id: postId },
-      include: [
-        {
-          model: PostGroupModel,
-          as: 'groups',
-          required: false,
-          attributes: ['groupId'],
-        },
-        {
-          model: MentionModel,
-          as: 'mentions',
-          required: false,
-          attributes: ['userId'],
-        },
-        {
-          model: MediaModel,
-          as: 'media',
-          required: false,
-          attributes: ['id', 'url', 'type', 'name', 'width', 'height', 'status'],
-        },
-        {
-          model: PostReactionModel,
-          as: 'ownerReactions',
-          required: false,
-          where: {
-            createdBy: user.id,
-          },
-        },
-      ],
-    });
-    if (!post) {
-      throw new LogicException(HTTP_STATUS_ID.APP_POST_NOT_FOUND);
-    }
-    await this.authorityService.checkCanReadPost(user, post);
-    let comments = null;
-    if (getArticleDto.withComment) {
-      comments = await this.commentService.getComments(
-        {
-          postId,
-          childLimit: getArticleDto.childCommentLimit,
-          order: getArticleDto.commentOrder,
-          childOrder: getArticleDto.childCommentOrder,
-          limit: getArticleDto.commentLimit,
-        },
-        user,
-        false
-      );
-    }
-    const jsonPost = post.toJSON();
-    await Promise.all([
-      this.reactionService.bindReactionToPosts([jsonPost]),
-      this.mentionService.bindMentionsToPosts([jsonPost]),
-      this.postService.bindActorToPost([jsonPost]),
-      this.postService.bindAudienceToPost([jsonPost]),
-    ]);
-
-    const result = this.classTransformer.plainToInstance(ArticleResponseDto, jsonPost, {
-      excludeExtraneousValues: true,
-    });
-    result['comments'] = comments;
-    return result;
-  }
+  ): Promise<any> {}
 
   /**
    * Get Public Post
@@ -141,66 +66,7 @@ export class ArticleService {
    * @returns Promise resolve ArticleResponseDto
    * @throws HttpException
    */
-  public async getPublicArticle(
-    postId: string,
-    getArticleDto?: GetArticleDto
-  ): Promise<ArticleResponseDto> {
-    const post = await this.postModel.findOne({
-      attributes: {
-        exclude: ['updatedBy'],
-      },
-      where: { id: postId },
-      include: [
-        {
-          model: PostGroupModel,
-          as: 'groups',
-          required: false,
-          attributes: ['groupId'],
-        },
-        {
-          model: MentionModel,
-          as: 'mentions',
-          required: false,
-          attributes: ['userId'],
-        },
-        {
-          model: MediaModel,
-          as: 'media',
-          required: false,
-          attributes: ['id', 'url', 'type', 'name', 'width', 'height', 'status'],
-        },
-      ],
-    });
-
-    if (!post) {
-      throw new LogicException(HTTP_STATUS_ID.APP_POST_NOT_FOUND);
-    }
-    await this.authorityService.checkPublicPost(post);
-    let comments = null;
-    if (getArticleDto.withComment) {
-      comments = await this.commentService.getComments({
-        postId,
-        childLimit: getArticleDto.childCommentLimit,
-        order: getArticleDto.commentOrder,
-        childOrder: getArticleDto.childCommentOrder,
-        limit: getArticleDto.commentLimit,
-      });
-    }
-    const jsonPost = post.toJSON();
-    await Promise.all([
-      this.reactionService.bindReactionToPosts([jsonPost]),
-      this.mentionService.bindMentionsToPosts([jsonPost]),
-      this.postService.bindActorToPost([jsonPost]),
-      this.postService.bindAudienceToPost([jsonPost]),
-    ]);
-
-    const result = this.classTransformer.plainToInstance(ArticleResponseDto, jsonPost, {
-      excludeExtraneousValues: true,
-    });
-
-    result['comments'] = comments;
-    return result;
-  }
+  public async getPublicArticle(postId: string, getArticleDto?: GetArticleDto): Promise<any> {}
 
   /**
    * Create Post
@@ -209,10 +75,7 @@ export class ArticleService {
    * @returns Promise resolve boolean
    * @throws HttpException
    */
-  public async createArticle(
-    authUser: UserDto,
-    createArticleDto: CreateArticleDto
-  ): Promise<IPost> {
+  public async createArticle(authUser: UserDto, createArticleDto: CreateArticleDto): Promise<any> {
     let transaction;
     try {
       const {
@@ -232,73 +95,10 @@ export class ArticleService {
       if (!creator) {
         ExceptionHelper.throwLogicException(HTTP_STATUS_ID.APP_USER_NOT_FOUND);
       }
-      const { groupIds } = audience;
-      await this.authorityService.checkCanCreatePost(authUser, groupIds);
 
-      if (categories && categories.length) {
-        //TODO
-        //await this.mentionService.checkValidMentions(groupIds, mentions);
-      }
-
-      if (hashtags && hashtags.length) {
-        //TODO
-        //await this.mentionService.checkValidMentions(groupIds, mentions);
-      }
-
-      if (series && series.length) {
-        //TODO
-        //await this.mentionService.checkValidMentions(groupIds, mentions);
-      }
-
-      if (mentions && mentions.length) {
-        await this.mentionService.checkValidMentions(groupIds, mentions);
-      }
-
-      const { files, images, videos } = media;
-      const uniqueMediaIds = [...new Set([...files, ...images, ...videos].map((i) => i.id))];
-      await this.mediaService.checkValidMedia(uniqueMediaIds, authUserId);
-      transaction = await this.sequelizeConnection.transaction();
-      const post = await this.postModel.create(
-        {
-          title,
-          summary,
-          isDraft: false,
-          content,
-          createdBy: authUserId,
-          updatedBy: authUserId,
-          isImportant: setting.isImportant,
-          importantExpiredAt: setting.isImportant === false ? null : setting.importantExpiredAt,
-          canShare: setting.canShare,
-          canComment: setting.canComment,
-          canReact: setting.canReact,
-          isProcessing: false,
-          isArticle: true,
-          views: 0,
-        },
-        { transaction }
-      );
-      if (uniqueMediaIds.length) {
-        await this.mediaService.sync(post.id, EntityType.POST, uniqueMediaIds, transaction);
-      }
-
-      await this.postService.addPostGroup(groupIds, post.id, transaction);
-
-      if (mentions.length) {
-        await this.mentionService.create(
-          mentions.map((userId) => ({
-            entityId: post.id,
-            userId,
-            mentionableType: MentionableType.POST,
-          })),
-          transaction
-        );
-      }
-
-      await transaction.commit();
-
-      return post;
+      //return post;
     } catch (error) {
-      if (typeof transaction !== 'undefined') await transaction.rollback();
+      // if (typeof transaction !== 'undefined') await transaction.rollback();
       this.logger.error(error, error?.stack);
       this.sentryService.captureException(error);
       throw error;
@@ -331,80 +131,7 @@ export class ArticleService {
         await this.postService.checkContent(updateArticleDto);
       }
       await this.postService.checkPostOwner(post, authUser.id);
-      const oldGroupIds = post.audience.groups.map((group) => group.id);
-      if (audience) {
-        await this.authorityService.checkCanUpdatePost(authUser, audience.groupIds);
-      }
-
-      if (mentions && mentions.length) {
-        await this.mentionService.checkValidMentions(
-          audience ? audience.groupIds : oldGroupIds,
-          mentions
-        );
-      }
-
-      const dataUpdate = {
-        updatedBy: authUserId,
-      };
-
-      if (content !== null) {
-        dataUpdate['content'] = content;
-      }
-      if (setting.hasOwnProperty('canShare')) {
-        dataUpdate['canShare'] = setting.canShare;
-      }
-      if (setting.hasOwnProperty('canComment')) {
-        dataUpdate['canComment'] = setting.canComment;
-      }
-      if (setting.hasOwnProperty('canReact')) {
-        dataUpdate['canReact'] = setting.canReact;
-      }
-      if (setting.hasOwnProperty('isImportant')) {
-        dataUpdate['isImportant'] = setting.isImportant;
-      }
-      if (setting.hasOwnProperty('importantExpiredAt')) {
-        dataUpdate['importantExpiredAt'] =
-          setting.isImportant === false ? null : setting.importantExpiredAt;
-      }
-      let newMediaIds = [];
-      transaction = await this.sequelizeConnection.transaction();
-      if (media) {
-        const { files, images, videos } = media;
-        newMediaIds = [...new Set([...files, ...images, ...videos].map((i) => i.id))];
-        await this.mediaService.checkValidMedia(newMediaIds, authUserId);
-        const mediaList =
-          newMediaIds.length === 0
-            ? []
-            : await this.mediaService.getMediaList({ where: { id: newMediaIds } });
-        if (
-          mediaList.filter(
-            (m) => m.status === MediaStatus.WAITING_PROCESS || m.status === MediaStatus.PROCESSING
-          ).length > 0
-        ) {
-          dataUpdate['isDraft'] = true;
-          dataUpdate['isProcessing'] = post.isDraft === true ? false : true;
-        }
-      }
-
-      await this.postModel.update(dataUpdate, {
-        where: {
-          id: post.id,
-          createdBy: authUserId,
-        },
-        transaction,
-      });
-
-      if (media) {
-        await this.mediaService.sync(post.id, EntityType.POST, newMediaIds, transaction);
-      }
-
-      if (mentions) {
-        await this.mentionService.setMention(mentions, MentionableType.POST, post.id, transaction);
-      }
-      if (audience && !ArrayHelper.arraysEqual(audience.groupIds, oldGroupIds)) {
-        await this.postService.setGroupByPost(audience.groupIds, post.id, transaction);
-      }
-      await transaction.commit();
+      // await transaction.commit();
 
       return true;
     } catch (error) {
