ac0da1a2c60214392b58430aecf6817d94719fc2
ylgr <quyen.luminous@gmail.com>
2022-06-01 01:23:17 +0700

    (+) follow test

--- src/modules/follow/tests/follow.service.spec.ts
+++ src/modules/follow/tests/follow.service.spec.ts
@@ -5,9 +5,17 @@ import { getModelToken } from '@nestjs/sequelize';
 import { FollowModel } from '../../../database/models/follow.model';
 import { InternalEventEmitterService } from '../../../app/custom/event-emitter';
 import { SentryService } from '@app/sentry';
+import {
+  createFollowDtoMock, emptyCreateFollowDtoMock,
+  emptyGroupIdsCreateFollowDtoMock,
+  emptyUserIdsCreateFollowDtoMock,
+} from './mocks/create-follow-dto.mock';
+import { emitKeypressEvents } from 'readline';
 
 describe('FollowService', () => {
-  let service: FollowService;
+  let followService: FollowService;
+  let internalEventEmitterService;
+  let sequelize;
   let model;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,12 +42,14 @@ describe('FollowService', () => {
           provide: getModelToken(FollowModel),
           useValue: {
             findOne: jest.fn(),
-            findAndCountAll: jest.fn(),
             findAll: jest.fn(),
             update: jest.fn(),
             create: jest.fn(),
             destroy: jest.fn(),
             findByPk: jest.fn(),
+            sequelize: {
+              query: jest.fn(),
+            }
           },
         },
         {
@@ -51,16 +61,44 @@ describe('FollowService', () => {
       ],
     }).compile();
 
-    service = module.get<FollowService>(FollowService);
+    followService = module.get<FollowService>(FollowService);
+    sequelize = module.get<Sequelize>(Sequelize);
+    internalEventEmitterService = module.get<InternalEventEmitterService>(InternalEventEmitterService);
     model = module.get<typeof FollowModel>(getModelToken(FollowModel))
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(followService).toBeDefined();
   });
+
   describe('FollowService.follow', () => {
     it('should sequelize query and event emit', async () => {
+      await followService.follow(createFollowDtoMock)
+      expect(model.sequelize.query).toBeCalled();
+      expect(internalEventEmitterService.emit).toBeCalled();
+    })
+    it('should sequelize query and event emit when empty user ids', async () => {
+      await followService.follow(emptyUserIdsCreateFollowDtoMock)
+      expect(model.sequelize.query).toBeCalled();
+      expect(internalEventEmitterService.emit).toBeCalled();
+    })
+    it('should sequelize query and event emit when empty group ids', async () => {
+      await followService.follow(emptyGroupIdsCreateFollowDtoMock)
+      expect(model.sequelize.query).toBeCalled();
+      expect(internalEventEmitterService.emit).toBeCalled();
+    })
+    it('should sequelize query and event emit when empty all', async () => {
+      await followService.follow(emptyCreateFollowDtoMock)
+      expect(model.sequelize.query).toBeCalled();
+      expect(internalEventEmitterService.emit).toBeCalled();
+    })
+  })
 
+  describe('FollowService.unfollow', () => {
+    it('should call destroy and emit', async () => {
+      await followService.unfollow(createFollowDtoMock)
+      expect(model.destroy).toBeCalled()
+      expect(internalEventEmitterService.emit).toBeCalled()
     })
   })
 
---
+++ src/modules/follow/tests/mocks/create-follow-dto.mock.ts
@@ -0,0 +1,16 @@
+export const createFollowDtoMock = {
+  userIds: [1, 2],
+  groupIds: [2, 3],
+};
+export const emptyUserIdsCreateFollowDtoMock = {
+  userIds: [],
+  groupIds: [2, 3],
+};
+export const emptyGroupIdsCreateFollowDtoMock = {
+  userIds: [1, 2],
+  groupIds: [],
+};
+export const emptyCreateFollowDtoMock = {
+  userIds: [],
+  groupIds: [],
+};
