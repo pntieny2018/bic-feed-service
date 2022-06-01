ce1482c1dce5e1baa76dfcd03a16bc200ac03400
ylgr <quyen.luminous@gmail.com>
2022-06-01 16:33:33 +0700

    (+) finish media module

--- src/modules/media/tests/media.controller.spec.ts
+++ src/modules/media/tests/media.controller.spec.ts
@@ -2,10 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MediaController } from '../media.controller';
 import { UploadService } from '../../upload';
 import { MediaService } from '../media.service';
+import { mockedUserAuth } from '../../post/test/mocks/user.mock';
+import { UploadType } from '../../upload/dto/requests/upload.dto';
+import { fileMock } from './mocks/file.mock';
 
 describe('MediaController', () => {
   let controller: MediaController;
-
+  let uploadServices;
+  let mediaServices;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MediaController],
@@ -27,6 +31,8 @@ describe('MediaController', () => {
     }).compile();
 
     controller = module.get<MediaController>(MediaController);
+    uploadServices = module.get<UploadService>(UploadService);
+    mediaServices = module.get<MediaService>(MediaService);
   });
 
   it('should be defined', () => {
@@ -35,8 +41,22 @@ describe('MediaController', () => {
 
   describe('MediaController.create', () => {
     it('should _uploadService.upload and _mediaService.create called', async () => {
+      uploadServices.upload.mockReturnValue('https://s3.aws.bein/asdfgh.png')
+      mediaServices.create.mockReturnValue({
+        toJSON: () => {
+        },
+      })
+      await controller.create(mockedUserAuth, fileMock, UploadType.POST_IMAGE)
+      expect(uploadServices.upload).toBeCalled()
+      expect(mediaServices.create).toBeCalled()
     });
   })
 
-
+  describe('MediaController.destroy', () => {
+    it('should _mediaService.destroy called', async () => {
+      mediaServices.destroy.mockReturnValue({})
+      await controller.destroy(mockedUserAuth,1)
+      expect(mediaServices.destroy).toBeCalled()
+    });
+  })
 });
--- src/modules/media/tests/mocks/create-media-dto.mock.ts
+++ src/modules/media/tests/mocks/create-media-dto.mock.ts
@@ -1,1 +1,13 @@
-export const createMediaDtoMock = {};
+import { UploadType } from '../../../upload/dto/requests/upload.dto';
+import { MediaStatus } from '../../../../database/models/media.model';
+
+export const createMediaDtoMock = {
+  url: 'https://s3.aws.bein/asdfgh.png',
+  uploadType: UploadType.POST_IMAGE,
+  name: 'asdfgh.png',
+  originName: 'asdfgh',
+  extension: 'png',
+  width: 500,
+  height: 500,
+  status: MediaStatus.COMPLETED,
+};
