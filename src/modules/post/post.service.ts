0682dc08a03fb21264303b1e075d9df3a127e1f2
Phi Dang <phidang@tgm.vn>
2022-06-07 10:41:48 +0700

    fix fail video

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1340,12 +1340,12 @@ export class PostService {
     const post = PostModel.tableName;
     const media = MediaModel.tableName;
     const query = ` UPDATE ${schema}.${post}
-                SET is_processing = tmp.is_processing, is_draft = tmp.isFailed
+                SET is_processing = tmp.is_processing, is_draft = tmp.isDraft
                 FROM (
                   SELECT pm.post_id, CASE WHEN SUM ( CASE WHEN m.status = '${MediaStatus.PROCESSING}' THEN 1 ELSE 0 END 
 		                ) >= 1 THEN true ELSE false END as is_processing,
                     CASE WHEN SUM ( CASE WHEN m.status = '${MediaStatus.FAILED}' OR m.status = '${MediaStatus.PROCESSING}' OR m.status = '${MediaStatus.WAITING_PROCESS}' THEN 1 ELSE 0 END 
-		                ) >= 1 THEN true ELSE false END as isFailed
+		                ) >= 1 THEN true ELSE false END as isDraft
                   FROM ${schema}.${media} as m
                   JOIN ${schema}.${postMedia} AS pm ON pm.media_id = m.id
                   WHERE pm.post_id = :postId
