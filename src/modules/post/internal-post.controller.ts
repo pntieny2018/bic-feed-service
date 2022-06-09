0530ca215b4211d2f49163974265a8f5a21936d4
ylgr <quyen.luminous@gmail.com>
2022-06-09 15:00:41 +0700

    (!) event

--- src/modules/post/internal-post.controller.ts
+++ src/modules/post/internal-post.controller.ts
@@ -9,7 +9,7 @@ export class InternalPostController {
   private _logger = new Logger(InternalPostController.name);
   public constructor(private _postSevice: PostService) {}
 
-  @EventPattern(KAFKA_TOPIC.BEIN_GROUP.USERS_FOLLOW_GROUPS)
+  @EventPattern(KAFKA_TOPIC.BEIN_GROUP.UPDATED_PRIVACY_GROUP)
   public async privacyUpdate(@Payload('value') updatePrivacyDto: UpdatePrivacyDto): Promise<void> {
     this._logger.debug(`[privacyUpdate]: ${JSON.stringify(updatePrivacyDto)}`);
     const postIds = await this._postSevice.findPostIdsByGroupId([updatePrivacyDto.groupId]);
