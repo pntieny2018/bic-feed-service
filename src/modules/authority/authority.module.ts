66d284d5cb246c5bee4526be04fb1cc38cfec3df
viet tu <quyen.luminous@gmail.com>
2022-07-13 17:43:09 +0700

    (-) user module anymore

--- src/modules/authority/authority.module.ts
+++ src/modules/authority/authority.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthorityService } from './authority.service';
 import { GroupModule } from '../../shared/group';
 import { AuthorityController } from './authority.controller';
-import { UserModule } from '../../shared/user';
 
 @Module({
-  imports: [GroupModule, UserModule],
+  imports: [GroupModule],
   controllers: [AuthorityController],
   providers: [AuthorityService],
   exports: [AuthorityService],
