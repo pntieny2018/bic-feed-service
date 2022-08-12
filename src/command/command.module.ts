60b78ee6edc7057ddf185c1caa64c54991834b2a
viet tu <quyen.luminous@gmail.com>
2022-08-12 19:31:19 +0700

    (+) script upload media domain

--- src/command/command.module.ts
+++ src/command/command.module.ts
@@ -10,15 +10,26 @@ import { GroupModule } from '../shared/group';
 import { PostModule } from '../modules/post';
 import { UpdatePrivacyPostCommand } from './update-post-privacy.command';
 import { MentionModule } from '../modules/mention';
+import { UpdateMediaDomainCommand } from './update-media-domain.command';
+import { MediaModule } from '../modules/media';
 
 @Module({
-  imports: [DatabaseModule, LibModule, UserModule, GroupModule, PostModule, MentionModule],
+  imports: [
+    DatabaseModule,
+    LibModule,
+    UserModule,
+    GroupModule,
+    PostModule,
+    MentionModule,
+    MediaModule,
+  ],
   providers: [
     SequelizeTinkerCommand,
     FixCommentCountCommand,
     FixPostCommentCountCommand,
     ReIndexEsPostCommand,
     UpdatePrivacyPostCommand,
+    UpdateMediaDomainCommand,
   ],
 })
 export class CommandModule {}
