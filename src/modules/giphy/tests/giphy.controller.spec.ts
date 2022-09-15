a6e8fdcefcc8ac225b953a8dc418d7d4615d2b15
viet tu <quyen.luminous@gmail.com>
2022-09-15 18:41:48 +0700

    (!) unit test

--- src/modules/giphy/tests/giphy.controller.spec.ts
+++ src/modules/giphy/tests/giphy.controller.spec.ts
@@ -2,24 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GiphyController } from '../giphy.controller';
 import { Rating } from '../dto/requests';
 import { HttpService } from '@nestjs/axios';
-import { AxiosResponse } from 'axios';
-import { of } from 'rxjs';
 import { SentryService } from '@app/sentry';
+import { giphyResMock } from './mocks/giphy-res.mock';
+import { HttpException } from '@nestjs/common';
 
 describe('GiphyController', () => {
   let controller: GiphyController;
   let httpService;
+  let sentryService;
   let axios;
 
-  const result: AxiosResponse = {
-    data: {
-      data: []
-    },
-    status: 200,
-    statusText: 'OK',
-    headers: {},
-    config: {}
-  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GiphyController],
@@ -44,6 +36,7 @@ describe('GiphyController', () => {
 
     controller = module.get<GiphyController>(GiphyController);
     httpService = module.get<HttpService>(HttpService);
+    sentryService = module.get<SentryService>(SentryService);
     axios = httpService.axiosRef
   });
 
@@ -53,17 +46,33 @@ describe('GiphyController', () => {
 
   describe('GiphyController.getTrending', () => {
     it('should httpService.get be called', async () => {
-      axios.get.mockResolvedValue(result);
+      axios.get.mockResolvedValue(giphyResMock);
       await controller.getTrending({limit: 25, rating: Rating.g})
       expect(axios.get).toBeCalled();
+    })
+    it('should sentry captureException', async () => {
+      try {
+        axios.get.mockRejectedValue(new HttpException('timeout', 500));
+        await controller.getTrending({limit: 25, rating: Rating.g})
+      } catch (e) {
+        expect(sentryService.captureException).toBeCalled();
+      }
     })
   })
 
   describe('GiphyController.search', () => {
     it('should httpService.get be called', async () => {
-      axios.get.mockResolvedValue(result);
+      axios.get.mockResolvedValue(giphyResMock);
       await controller.search({limit: 25, rating: Rating.g, q: 'bla bla'})
       expect(axios.get).toBeCalled();
+    })
+    it('should sentry captureException', async () => {
+      try {
+        axios.get.mockRejectedValue(new HttpException('timeout', 500));
+        await controller.search({limit: 25, rating: Rating.g, q: 'bla bla'})
+      } catch (e) {
+        expect(sentryService.captureException).toBeCalled();
+      }
     })
   })
 })
