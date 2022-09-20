2b9f4d30a9ef43785518cf609d124c23f8034ce2
Phi Dang <phidang@tgm.vn>
2022-09-20 14:23:01 +0700

    BEIN-8519

--- src/database/models/post.model.ts
+++ src/database/models/post.model.ts
@@ -680,7 +680,9 @@ export class PostModel extends Model<IPost, Optional<IPost, 'id'>> implements IP
         WHERE r.post_id = p.id AND r.user_id = :authUserId ), false
       ) AS "markedReadPost"`;
     } else {
-      subSelect += `, false "markedReadPost"`;
+      subSelect += `, COALESCE((SELECT true FROM ${schema}.${userMarkReadPostTable} as r
+        WHERE r.post_id = p.id AND r.user_id = :authUserId ), false
+      ) AS "markedReadPost"`;
     }
 
     const subQueryGetPosts = `
