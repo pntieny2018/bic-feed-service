38c71dcfa67776bb21f45352ef82d81d53c1b8f0
Phi Dang <phidang@tgm.vn>
2022-11-02 16:48:06 +0700

    add search post type

--- src/common/interfaces/body-ealsticsearch.interface.ts
+++ src/common/interfaces/body-ealsticsearch.interface.ts
@@ -2,6 +2,8 @@ type BoolAttr = {
   must: any[];
   filter: any[];
   should: any[];
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  minimum_should_match: number;
 };
 export type BodyES = {
   query: {
