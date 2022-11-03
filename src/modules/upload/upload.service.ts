1cd85cc79eef7be75ef585285e2d7c32c4104b70
Phi Dang <phidang@tgm.vn>
2022-11-03 14:40:43 +0700

    debug upload

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -5,7 +5,7 @@ import { IS3Config } from '../../config/s3';
 import { ConfigService } from '@nestjs/config';
 import { UploadPrefix } from './dto/requests/upload.dto';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-
+import { fromContainerMetadata } from '@aws-sdk/credential-providers';
 @Injectable()
 export class UploadService {
   private _storage: S3Client;
@@ -16,6 +16,10 @@ export class UploadService {
     this._s3Config = s3Config;
     this._storage = new S3Client({
       region: s3Config.region,
+      credentials: fromContainerMetadata({
+        timeout: 1000,
+        maxRetries: 0,
+      }),
     });
   }
 
@@ -38,7 +42,7 @@ export class UploadService {
       this.logger.debug(
         '==S3Debug==',
         JSON.stringify({
-          config: this._s3Config
+          config: this._s3Config,
         })
       );
       await this._storage.send(
