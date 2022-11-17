fde067a9cee5d91d295730d33c91ac04600ca4d2
Phi Dang <phidang@tgm.vn>
2022-11-17 15:03:02 +0700

    BEIN-9705-count-number-of-post

--- src/modules/internal/dto/requests/get-total-post-in-groups.ts
+++ src/modules/internal/dto/requests/get-total-post-in-groups.ts
@@ -6,6 +6,7 @@ import { validate as isValidUUID } from 'uuid';
 export class GetTotalPostsInGroupDto {
   @ApiProperty({
     type: String,
+    name: 'group_ids',
     example: '9322c384-fd8e-4a13-80cd-1cbd1ef95ba8,986dcaf4-c1ea-4218-b6b4-e4fd95a3c28e',
   })
   @IsNotEmpty()
