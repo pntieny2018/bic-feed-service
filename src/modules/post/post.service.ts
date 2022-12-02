b1fcbb544e06bb764ee90feb3ca2163fb2f81055
Phi Dang <phidang@tgm.vn>
2022-12-02 12:45:07 +0700

    BEIN-10411 [BUG][BE/Web] Member vẫn Edit/Delete được post/article khi không có quyền

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -245,7 +245,7 @@ export class PostService {
 
   public async getListWithGroupsByIds(postIds: string[], must: boolean): Promise<IPost[]> {
     const postGroups = await this.postModel.findAll({
-      attributes: ['id', 'title', 'lang', 'createdBy'],
+      attributes: ['id', 'title', 'lang', 'isDraft', 'createdBy'],
       include: [
         {
           model: PostGroupModel,
