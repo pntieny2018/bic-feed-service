28d2376a4ef8dd1726ccc40e6ee9c0445b87fadd
The <thevan@tgm.vn>
2022-12-15 18:27:29 +0700

    BEIN-9926_BEIN-10530 fix payload get item report of me

--- src/modules/report-content/dto/get-blocked-content-of-me.dto.ts
+++ src/modules/report-content/dto/get-blocked-content-of-me.dto.ts
@@ -1,16 +1,18 @@
-import { IsArray, IsOptional } from 'class-validator';
-import { Expose } from 'class-transformer';
+import { IsArray, IsOptional, IsUUID } from 'class-validator';
+import { Expose, Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 import { PageOptionsDto } from '../../../common/dto';
 
 export class GetBlockedContentOfMeDto extends PageOptionsDto {
   @ApiProperty({
     name: 'target_ids',
+    required: false,
   })
   @Expose({
     name: 'target_ids',
   })
   @IsArray()
   @IsOptional()
+  @IsUUID('4', { each: true })
   public specTargetIds?: string[];
 }
