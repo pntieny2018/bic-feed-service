5f6e687ac0f922231db594dde40ca6c20f4ae41f
Phi Dang <phidang@tgm.vn>
2022-12-16 14:17:55 +0700

    BEIN-10474-report-content

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -180,7 +180,6 @@ export class PostService {
     if (user) {
       condition = {
         id: postId,
-        isHidden: false,
         [Op.or]: [{ isDraft: false }, { isDraft: true, createdBy: user.id }],
       };
     } else {
@@ -193,7 +192,7 @@ export class PostService {
       include,
     });
 
-    if (!post) {
+    if (!post || (post.isHidden === true && post.createdBy !== user?.id)) {
       throw new LogicException(HTTP_STATUS_ID.APP_POST_NOT_EXISTING);
     }
 
