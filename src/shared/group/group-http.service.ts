9345ff61cf0568eea9f50ae5a16408f534ddf09b
The <thevan@tgm.vn>
2022-12-19 14:31:24 +0700

    BEIN-9926_BEIN-10530 fix param define when get list admin of community

--- src/shared/group/group-http.service.ts
+++ src/shared/group/group-http.service.ts
@@ -9,21 +9,23 @@ export class GroupHttpService {
 
   public constructor(private readonly _httpService: HttpService) {}
 
-  public async getAdminIds(rootGroupIds: string[]): Promise<Record<string, string[]>> {
+  public async getAdminIds(
+    rootGroupIds: string[],
+    offset = 0,
+    limit = 50
+  ): Promise<Record<string, string[]>> {
     try {
-      const params = {
-        ['root_group_ids']: rootGroupIds.join(','),
-        offset: 0,
-        limit: 50,
-      };
+      const params = `root_group_ids=${rootGroupIds.join(',')}&offset=${offset}&limit=${limit}`;
+
       const response = await lastValueFrom(
-        this._httpService.get(COMMUNITY_ADMIN_PATH, {
+        this._httpService.get(`${COMMUNITY_ADMIN_PATH}?${params}`, {
           params: params,
         })
       );
       if (response.status !== HttpStatus.OK) {
         return null;
       }
+      this._logger.debug(response.data);
       return response.data['data'];
     } catch (ex) {
       this._logger.error(ex);
