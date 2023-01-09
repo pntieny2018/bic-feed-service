4540a51ac55907500d1edd198c11f2f2baa7848f
The <thevan@tgm.vn>
2023-01-09 13:44:55 +0700

    BEIN-10867_BEIN-10931 user get detail comment was reported

--- src/modules/report-content/dto/get-blocked-content-of-me.dto.ts
+++ src/modules/report-content/dto/get-blocked-content-of-me.dto.ts
@@ -1,7 +1,8 @@
-import { IsArray, IsOptional, IsUUID } from 'class-validator';
+import { IsArray, IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 import { Expose, Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 import { PageOptionsDto } from '../../../common/dto';
+import { TargetType } from '../contstants';
 
 export class GetBlockedContentOfMeDto extends PageOptionsDto {
   @ApiProperty({
@@ -15,4 +16,15 @@ export class GetBlockedContentOfMeDto extends PageOptionsDto {
   @IsOptional()
   @IsUUID('4', { each: true })
   public specTargetIds?: string[];
+
+  @ApiProperty({
+    example: Object.values(TargetType).join(','),
+    name: 'target_type',
+    required: false,
+  })
+  @Expose({
+    name: 'target_type',
+  })
+  @IsNotEmpty()
+  public targetType?: TargetType;
 }
