44fa920d38513316a35f3c0525361fa2e9e84d22
viet tu <quyen.luminous@gmail.com>
2023-01-17 19:21:23 +0700

    BEIN-11352: remove test func

--- src/modules/internal/internal.service.ts
+++ src/modules/internal/internal.service.ts
@@ -1,16 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PostService } from '../post/post.service';
-import { PostStatus } from '../../database/models/post.model';
-import { StateVerb } from '../post/dto/requests/update-state.dto';
-import { PostUpdateCacheGroupEvent } from '../../events/post/post-update-cache-group.event';
-import { InternalEventEmitterService } from '../../app/custom/event-emitter';
 
 @Injectable()
 export class InternalService {
-  public constructor(
-    private _postService: PostService,
-    private _eventEmitter: InternalEventEmitterService
-  ) {}
+  public constructor(private _postService: PostService) {}
 
   public async getTotalPostByGroupIds(groupIds: string[]): Promise<any> {
     return this._postService.getTotalPostByGroupIds(groupIds);
