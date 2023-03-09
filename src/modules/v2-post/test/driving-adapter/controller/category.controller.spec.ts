580ac632d38fa12d1ebe70b6fd1f222c155b1974
viet tu <quyen.luminous@gmail.com>
2023-03-09 14:45:02 +0700

    BEIN-12358: category controller test

---
+++ src/modules/v2-post/test/driving-adapter/controller/category.controller.spec.ts
@@ -0,0 +1,64 @@
+import { CategoryController } from '../../../driving-apdater/controller/category.controller';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Test, TestingModule } from '@nestjs/testing';
+import { userMock } from '../../mock/user.dto.mock';
+
+describe('CategoryController', () => {
+  let controller: CategoryController;
+  let command: CommandBus;
+  let query: QueryBus;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CategoryController, CommandBus, QueryBus],
+    }).compile();
+
+    controller = module.get<CategoryController>(CategoryController);
+    command = module.get<CommandBus>(CommandBus);
+    query = module.get<QueryBus>(QueryBus);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const categoryMocks = [
+    {
+      "id": "6b9fbc18-04c3-4a4a-bd64-453add6724dd",
+      "name": "Outdoors",
+      "slug": "outdoors",
+      "created_at": "2022-09-21T08:33:18.881Z"
+    },
+    {
+      "id": "ce8c7613-426d-48e4-8d74-c2a12adc05f9",
+      "name": "Fashion & Beauty",
+      "slug": "fashion-beauty",
+      "created_at": "2022-09-21T08:33:18.881Z"
+    }
+  ]
+
+  describe('Get', () => {
+
+    it('Should get categories successfully', async () => {
+      jest.spyOn(query, 'execute').mockResolvedValue({ rows: categoryMocks, total: 0 });
+      const result = await controller.get(userMock, {});
+      expect(result).toEqual({
+        list: [
+          {
+            "id": "6b9fbc18-04c3-4a4a-bd64-453add6724dd",
+            "name": "Outdoors",
+            "slug": "outdoors",
+          },
+          {
+            "id": "ce8c7613-426d-48e4-8d74-c2a12adc05f9",
+            "name": "Fashion & Beauty",
+            "slug": "fashion-beauty",
+          }
+        ],
+        "meta": {
+          "hasNextPage": false,
+          "total": 0
+        }
+      });
+    });
+  });
+});
