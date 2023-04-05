7ad5b54c83fa741af8c21b2993fe5cf32e638d8c
Phi Dang <phidang@tgm.vn>
2023-04-05 16:11:20 +0700

    feat: BEIN-13445 change endpoint API

--- src/modules/post/content.controller.ts
+++ src/modules/post/content.controller.ts
@@ -89,7 +89,7 @@ export class ContentController {
     return this._postAppService.getAudience(postId, user, getAudienceContentDto);
   }
 
-  @ApiOperation({ summary: 'Pin content' })
+  @ApiOperation({ summary: 'Pin/unpin content' })
   @ApiOkResponse({
     type: PostResponseDto,
   })
