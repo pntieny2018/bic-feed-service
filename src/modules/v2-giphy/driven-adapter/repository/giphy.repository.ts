386f57f5d77e6de9359487d31a3ab2fd4df6393c
viet tu <quyen.luminous@gmail.com>
2023-04-07 20:19:55 +0700

    fix: BEIN-13160 remove giphy helper, move to repository

--- src/modules/v2-giphy/driven-adapter/repository/giphy.repository.ts
+++ src/modules/v2-giphy/driven-adapter/repository/giphy.repository.ts
@@ -3,11 +3,8 @@ import { IGiphyRepository } from './interface/giphy.repository.interface';
 import { HttpService } from '@nestjs/axios';
 import { GiphyEntity } from '../../domain/giphy.entity';
 import { lastValueFrom } from 'rxjs';
-import {
-  DEFAULT_GIPHY_REQUEST_TIME_OUT,
-  GifType,
-  GiphyHelper,
-} from '../../../../common/helpers/giphy.helper';
+import { GifType } from '../../data-type';
+export const DEFAULT_GIPHY_REQUEST_TIME_OUT = 4500; // 4,5s
 
 @Injectable()
 export class GiphyRepository implements IGiphyRepository {
@@ -32,7 +29,7 @@ export class GiphyRepository implements IGiphyRepository {
       );
       if (response.status === 200) {
         return response.data.data.map((e) => {
-          const detailInfo = GiphyHelper.getGiphyDetailInfo(e.id, GifType[type], e.images);
+          const detailInfo = this.getGiphyDetailInfo(e.id, GifType[type], e.images);
           return new GiphyEntity({
             id: e.id,
             type: e.type,
@@ -73,7 +70,7 @@ export class GiphyRepository implements IGiphyRepository {
       );
       if (response.status === 200) {
         return response.data.data.map((e) => {
-          const detailInfo = GiphyHelper.getGiphyDetailInfo(e.id, GifType[type], e.images);
+          const detailInfo = this.getGiphyDetailInfo(e.id, GifType[type], e.images);
           return new GiphyEntity({
             id: e.id,
             type: e.type,
@@ -88,5 +85,35 @@ export class GiphyRepository implements IGiphyRepository {
       this._logger.error(e);
     }
     return [];
+  }
+
+  public getGiphyDetailInfo(
+    id?: string,
+    type: GifType = GifType.PREVIEW_GIF,
+    images?: object
+  ): { url: string; height: string; width: string; size: string } {
+    if (!id) return null;
+    if (type === GifType.ORIGINAL) {
+      return {
+        url: 'https://i.giphy.com/' + id + '.gif',
+        height: images['original'] ? images['original'].height : null,
+        width: images['original'] ? images['original'].width : null,
+        size: images['original'] ? images['original'].size : null,
+      };
+    } else if (type === GifType.PREVIEW_GIF) {
+      return {
+        url: 'https://i.giphy.com/media/' + id + '/giphy.gif',
+        height: images['preview_gif'] ? images['preview_gif'].height : null,
+        width: images['preview_gif'] ? images['preview_gif'].width : null,
+        size: images['preview_gif'] ? images['preview_gif'].size : null,
+      };
+    } else if (type === GifType.PREVIEW_WEBP) {
+      return {
+        url: 'https://i.giphy.com/media/' + id + '/giphy.webp',
+        height: images['preview_webp'] ? images['preview_webp'].height : null,
+        width: images['preview_webp'] ? images['preview_webp'].width : null,
+        size: images['preview_webp'] ? images['preview_webp'].size : null,
+      };
+    }
   }
 }
