fa681222dc2682d43c2cb1d4531b3ef0c741222b
thanhphat-pic <thanhphat@evol.vn>
2023-05-09 09:23:41 +0700

    feat: BEIN-14519 move const variables data to mock data file

--- src/modules/v2-group/tests/application/group.app-service.spec.ts
+++ src/modules/v2-group/tests/application/group.app-service.spec.ts
@@ -6,8 +6,14 @@ import {
   IGroupRepository,
 } from '../../domain/repositoty-interface/group.repository.interface';
 import { GroupRepository } from '../../driven-adapter/repository/group.repository';
-import { groupMock } from '../mocks/group.mock';
-import { GroupEntity, GroupProps } from '../../domain/model/group';
+import {
+  groupDto,
+  groupListMock,
+  groupMock,
+  groupPropsMock,
+  userMocked,
+} from '../mocks/group.mock';
+import { GroupEntity } from '../../domain/model/group';
 import { GroupPrivacy } from '../../data-type';
 
 describe('GroupApplicationService', () => {
@@ -33,21 +39,6 @@ describe('GroupApplicationService', () => {
   });
 
   describe('GroupService.findOne', () => {
-    const groupPropsMock: GroupProps = {
-      id: '22ccce09-0cea-4984-be0f-44e8839ac52b',
-      name: 'Love TS',
-      icon: 'ts',
-      privacy: GroupPrivacy.OPEN,
-      rootGroupId: '855bedeb-b708-4e13-8c68-131d92cd7911',
-      communityId: '855bedeb-b708-4e13-8c68-131d92cd7912',
-      isCommunity: false,
-      child: {
-        open: ['d3a2e019-1dba-485d-a8cf-ec037e9f25af', 'd3bc1d0d-0511-43e7-acfa-197a7aeab7eb'],
-        closed: ['c4d5c2be-86f5-4db2-8959-af92ff5ae469', '4878cd75-16c0-40cc-84e4-ebc42bda2d99'],
-        private: ['1d3251ef-f520-4703-b1e9-df29db4d6a9f', 'df98b0f0-c2b7-41dd-a4ee-28fea1e83231'],
-        secret: ['d3a2e019-1dba-485d-a8cf-ec037e9f25a1', 'd3bc1d0d-0511-43e7-acfa-197a7aeab7e2'],
-      },
-    };
     const groupEntityMocked = new GroupEntity(groupPropsMock);
     it('Should returned a GroupDto', async () => {
       jest.spyOn(repo, 'findOne').mockResolvedValue(groupEntityMocked);
@@ -57,40 +48,8 @@ describe('GroupApplicationService', () => {
   });
 
   describe('GroupService.findAllByIds', () => {
-    const groupPropsMock: GroupProps[] = [
-      {
-        id: '22ccce09-0cea-4984-be0f-44e8839ac52b',
-        name: 'Love TS',
-        icon: 'ts',
-        privacy: GroupPrivacy.OPEN,
-        rootGroupId: '855bedeb-b708-4e13-8c68-131d92cd7911',
-        communityId: '855bedeb-b708-4e13-8c68-131d92cd7912',
-        isCommunity: false,
-        child: {
-          open: ['d3a2e019-1dba-485d-a8cf-ec037e9f25af', 'd3bc1d0d-0511-43e7-acfa-197a7aeab7eb'],
-          closed: ['c4d5c2be-86f5-4db2-8959-af92ff5ae469', '4878cd75-16c0-40cc-84e4-ebc42bda2d99'],
-          private: ['1d3251ef-f520-4703-b1e9-df29db4d6a9f', 'df98b0f0-c2b7-41dd-a4ee-28fea1e83231'],
-          secret: ['d3a2e019-1dba-485d-a8cf-ec037e9f25a1', 'd3bc1d0d-0511-43e7-acfa-197a7aeab7e2'],
-        },
-      },
-      {
-        id: 'cc5df28b-885b-45a5-b44d-389b0f827c8a',
-        name: 'Love Golang',
-        icon: 'gl',
-        privacy: GroupPrivacy.CLOSED,
-        rootGroupId: '855bedeb-b708-4e13-8c68-131d92cd7911',
-        communityId: '855bedeb-b708-4e13-8c68-131d92cd7912',
-        isCommunity: false,
-        child: {
-          open: ['aac7a9ee-1432-4420-86d0-64a663e61123', '3e4822ee-063f-4029-86ca-2e98fae3c708'],
-          closed: [],
-          private: [],
-          secret: [],
-        },
-      },
-    ];
-    const groupEntitiesMocked = groupPropsMock.map((item) => new GroupEntity(item));
-    const groupIds = groupPropsMock.map((item) => item.id);
+    const groupEntitiesMocked = groupListMock.map((item) => new GroupEntity(item));
+    const groupIds = groupListMock.map((item) => item.id);
     it('Should returned a list of GroupDto', async () => {
       jest.spyOn(repo, 'findAllByIds').mockResolvedValue(groupEntitiesMocked);
       const result = await groupAppService.findAllByIds(groupIds);
@@ -99,40 +58,8 @@ describe('GroupApplicationService', () => {
   });
 
   describe('GroupService.getGroupIdAndChildIdsUserJoined', () => {
-    const userMocked = {
-      id: '7251dac7-5088-4a33-b900-d1b058edaf98',
-      username: 'martine.baumbach',
-      avatar: 'https://bein.group/baumbach.png',
-      email: 'baumbach@tgm.vn',
-      fullname: 'Martine Baumbach',
-      groups: [
-        'c4d5c2be-86f5-4db2-8959-af92ff5ae469',
-        '9b42ac09-e9b9-4899-9a72-3a0832693ea4',
-        'bc04d99e-97e5-42ef-9006-1448a5d05f85',
-        'e2487d02-b7be-4185-8245-f7596eba1437',
-      ],
-    };
-    const groupMocked: GroupDto = {
-      child: {
-        closed: ['c4d5c2be-86f5-4db2-8959-af92ff5ae469', '4878cd75-16c0-40cc-84e4-ebc42bda2d99'],
-        open: ['234de9dd-d84c-421b-bd52-90d982753427', '9b42ac09-e9b9-4899-9a72-3a0832693ea4'],
-        secret: [],
-        private: ['1d3251ef-f520-4703-b1e9-df29db4d6a9f', 'df98b0f0-c2b7-41dd-a4ee-28fea1e83231'],
-      },
-      id: 'e2487d02-b7be-4185-8245-f7596eba1437',
-      name: '321 matrix',
-      icon: 'https://media.beincom.io/image/variants/group/avatar/839b0cb1-a0de-4045-90df-c4ecae39326b',
-      privacy: GroupPrivacy.CLOSED,
-      rootGroupId: 'e2487d02-b7be-4185-8245-f7596eba1437',
-      communityId: '3c5e58f2-ee1d-4292-bf9a-c60cfa3cefe1',
-      isCommunity: true,
-    };
-
     it('Should returned be true', () => {
-      const result = groupAppService.getGroupIdAndChildIdsUserJoined(
-        groupMocked,
-        userMocked.groups
-      );
+      const result = groupAppService.getGroupIdAndChildIdsUserJoined(groupDto, userMocked.groups);
       expect(result).toEqual([
         'e2487d02-b7be-4185-8245-f7596eba1437',
         '9b42ac09-e9b9-4899-9a72-3a0832693ea4',
@@ -142,58 +69,21 @@ describe('GroupApplicationService', () => {
   });
 
   describe('GroupService.getGroupIdAndChildIdsUserJoined.OPEN', () => {
-    const userMocked = {
-      id: 'fe26b6bc-0253-49b2-9eb4-3c1545650548',
-      username: 'hoangnhut',
-      avatar:
-        'https://media.beincom.com/image/variants/user/avatar/f98fd465-12ff-4f39-93b4-7ecf42befd7e',
-      email: 'nhutduong@tgm.vn',
-      fullname: 'Dương Hoàng Nhựt',
-      groups: ['c4d5c2be-86f5-4db2-8959-af92ff5ae469', '9b42ac09-e9b9-4899-9a72-3a0832693ea4'],
-    };
-    const groupMocked: GroupDto = {
-      child: {
-        closed: ['c4d5c2be-86f5-4db2-8959-af92ff5ae469', '4878cd75-16c0-40cc-84e4-ebc42bda2d99'],
-        open: ['234de9dd-d84c-421b-bd52-90d982753427', '9b42ac09-e9b9-4899-9a72-3a0832693ea4'],
-        secret: [],
-        private: ['1d3251ef-f520-4703-b1e9-df29db4d6a9f', 'df98b0f0-c2b7-41dd-a4ee-28fea1e83231'],
-      },
-      id: 'e2487d02-b7be-4185-8245-f7596eba1437',
-      name: '321 matrix',
-      icon: 'https://media.beincom.io/image/variants/group/avatar/839b0cb1-a0de-4045-90df-c4ecae39326b',
-      privacy: GroupPrivacy.OPEN,
-      rootGroupId: 'e2487d02-b7be-4185-8245-f7596eba1437',
-      communityId: '3c5e58f2-ee1d-4292-bf9a-c60cfa3cefe1',
-      isCommunity: true,
-    };
-
+    const cloesedGroupMocked: GroupDto = { ...groupDto, privacy: GroupPrivacy.OPEN };
     it('Should returned be true', () => {
       const result = groupAppService.getGroupIdAndChildIdsUserJoined(
-        groupMocked,
+        cloesedGroupMocked,
         userMocked.groups
       );
       expect(result).toEqual([
+        'e2487d02-b7be-4185-8245-f7596eba1437',
         '9b42ac09-e9b9-4899-9a72-3a0832693ea4',
         'c4d5c2be-86f5-4db2-8959-af92ff5ae469',
-        'e2487d02-b7be-4185-8245-f7596eba1437',
       ]);
     });
   });
 
   describe('GroupService.getGroupAdminIds', () => {
-    const userMocked = {
-      id: '7251dac7-5088-4a33-b900-d1b058edaf98',
-      username: 'martine.baumbach',
-      avatar: 'https://bein.group/baumbach.png',
-      email: 'baumbach@tgm.vn',
-      fullname: 'Martine Baumbach',
-      groups: [
-        'c4d5c2be-86f5-4db2-8959-af92ff5ae469',
-        '9b42ac09-e9b9-4899-9a72-3a0832693ea4',
-        'bc04d99e-97e5-42ef-9006-1448a5d05f85',
-        'e2487d02-b7be-4185-8245-f7596eba1437',
-      ],
-    };
     it('Should returned list adminIds', async () => {
       jest.spyOn(repo, 'getGroupAdminIds').mockResolvedValue([userMocked.id]);
       const result = await groupAppService.getGroupAdminIds(userMocked, userMocked.groups);
@@ -202,7 +92,6 @@ describe('GroupApplicationService', () => {
   });
 
   describe('GroupService.getAdminIds', () => {
-    const rootGroupIds = ['c567c88e-38a4-4859-b067-cf91002c5963/'];
     const groupAdminsResponse = {
       code: 'api.ok',
       meta: {
@@ -221,9 +110,8 @@ describe('GroupApplicationService', () => {
     };
     it('Should returned list adminIds', async () => {
       jest.spyOn(repo, 'getAdminIds').mockResolvedValue(groupAdminsResponse.data);
-      const result = await groupAppService.getAdminIds(rootGroupIds);
+      const result = await groupAppService.getAdminIds(groupMock.map((item) => item.id));
       expect(result).toEqual(groupAdminsResponse.data);
     });
   });
-  
 });
