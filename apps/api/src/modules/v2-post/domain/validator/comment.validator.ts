fdc21c8e14d603caa9aff8f7335c7b51db9ac467
thanhphat-pic <thanhphat@evol.vn>
2023-05-15 18:10:05 +0700

    feat: BEIN-12361 remove unused imported library

--- apps/api/src/modules/v2-post/domain/validator/comment.validator.ts
+++ apps/api/src/modules/v2-post/domain/validator/comment.validator.ts
@@ -1,23 +1,11 @@
-import { BadRequestException, ForbiddenException, Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { HTTP_STATUS_ID } from '../../../../common/constants';
-import { LogicException } from '../../../../common/exceptions';
-import {
-  IUserApplicationService,
-  USER_APPLICATION_TOKEN,
-  UserDto,
-} from '../../../v2-user/application';
 import { PostAllow } from '../../data-type/post-allow.enum';
 import { ICommentValidator } from './interface/comment.validator.interface';
-import { UserMentionDto } from '../../application/dto';
 import { ContentEntity } from '../model/content/content.entity';
 
 @Injectable()
 export class CommentValidator implements ICommentValidator {
-  public constructor(
-    @Inject(USER_APPLICATION_TOKEN)
-    private _userAppService: IUserApplicationService
-  ) {}
-
   /**
    * Check post policy
    * @param post PostEntity
@@ -34,48 +22,5 @@ export class CommentValidator implements ICommentValidator {
             : `Comment of this post are not available`,
       });
     }
-  }
-
-  /**
-   * Check Valid Mentions
-   * @param groupIds
-   * @param userIds number[]
-   * @return users UserDto[]
-   * @throws LogicException
-   */
-  public async checkValidMentionsAndReturnUsers(
-    groupIds: string[],
-    userIds: string[]
-  ): Promise<UserDto[]> {
-    const users = await this._userAppService.findAllByIds(userIds, { withGroupJoined: true });
-    for (const user of users) {
-      if (!groupIds.some((groupId) => user.groups.includes(groupId))) {
-        throw new LogicException(HTTP_STATUS_ID.API_FORBIDDEN);
-      }
-    }
-    return users;
-  }
-
-  /**
-   * Map mentions to UserInfo
-   * @param mentions string[]
-   * @param users UserDto[]
-   * @throws BadRequestException
-   * returns UserMentionDto
-   */
-  public mapMentionWithUserInfo(mentions: string[], users: UserDto[]): UserMentionDto {
-    if (users.length < mentions.length) {
-      throw new BadRequestException('Mention users not found or resolved');
-    }
-    return users.reduce((returnValue, current) => {
-      return {
-        ...returnValue,
-        [current.username]: {
-          id: current.id,
-          username: current.username,
-          fullname: current.fullname,
-        },
-      };
-    }, {});
   }
 }
