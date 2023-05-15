ff6a8d15b3670c20d14737c2314f344ff56e4a38
thanhphat-pic <thanhphat@evol.vn>
2023-05-15 18:50:46 +0700

    feat: BEIN-12361 remove unused function

--- apps/api/src/modules/v2-post/domain/validator/comment.validator.ts
+++ apps/api/src/modules/v2-post/domain/validator/comment.validator.ts
@@ -1,26 +1,5 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
-import { HTTP_STATUS_ID } from '../../../../common/constants';
-import { PostAllow } from '../../data-type/post-allow.enum';
+import { Injectable } from '@nestjs/common';
 import { ICommentValidator } from './interface/comment.validator.interface';
-import { ContentEntity } from '../model/content/content.entity';
 
 @Injectable()
-export class CommentValidator implements ICommentValidator {
-  /**
-   * Check post policy
-   * @param post PostEntity
-   * @param action PostAllow
-   * @returns Promise resolve boolean
-   */
-  public allowAction(post: ContentEntity, action: PostAllow): void {
-    if (!post.get('setting')[action]) {
-      throw new ForbiddenException({
-        code: HTTP_STATUS_ID.API_FORBIDDEN,
-        message:
-          action === PostAllow.REACT
-            ? `React of this post are not available`
-            : `Comment of this post are not available`,
-      });
-    }
-  }
-}
+export class CommentValidator implements ICommentValidator {}
--- apps/api/src/modules/v2-post/domain/validator/interface/comment.validator.interface.ts
+++ apps/api/src/modules/v2-post/domain/validator/interface/comment.validator.interface.ts
@@ -1,8 +1,3 @@
-import { PostAllow } from '../../../data-type/post-allow.enum';
-import { ContentEntity } from '../../model/content/content.entity';
-
-export interface ICommentValidator {
-  allowAction(post: ContentEntity, action: PostAllow): void;
-}
+export interface ICommentValidator {}
 
 export const COMMENT_VALIDATOR_TOKEN = 'COMMENT_VALIDATOR_TOKEN';
