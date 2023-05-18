a4bea89108e71fe4603ac80119ab3610057d64d4
The <thevan@tgm.vn>
2023-05-19 00:38:25 +0700

    fix: BEIN-14757 condition for notify

--- src/listeners/post/post.listener.ts
+++ src/listeners/post/post.listener.ts
@@ -383,7 +383,7 @@ export class PostListener {
         }
       });
 
-      if (sameOwnerItems.length) {
+      if (sameOwnerItems.length && !newPost.isHidden) {
         sameOwnerItems.forEach((so) => {
           this._internalEventEmitter.emit(
             new SeriesChangedItemsEvent({
@@ -411,7 +411,7 @@ export class PostListener {
           new SeriesAddedItemsEvent({
             itemIds: [newPost.id],
             seriesId: seriesId,
-            skipNotify: skipNotifyForNewItems.includes(seriesId),
+            skipNotify: skipNotifyForNewItems.includes(seriesId) && !newPost.isHidden,
             actor: actor,
             context: 'publish',
           })
@@ -437,7 +437,7 @@ export class PostListener {
               },
             ],
             seriesId: seriesId,
-            skipNotify: skipNotifyForRemoveItems.includes(seriesId),
+            skipNotify: skipNotifyForRemoveItems.includes(seriesId) && !newPost.isHidden,
             actor: actor,
             contentIsDeleted: false,
           })
