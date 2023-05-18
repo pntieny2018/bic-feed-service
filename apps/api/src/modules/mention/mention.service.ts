1a69ccefc441cb425e7926a409eddc7ef2061ba5
Phi Dang <phidang@tgm.vn>
2023-05-18 13:55:18 +0700

    feat: BEIN-13163 fix comment mentions

--- apps/api/src/modules/mention/mention.service.ts
+++ apps/api/src/modules/mention/mention.service.ts
@@ -66,7 +66,6 @@ export class MentionService {
    */
   public async bindToComment(commentsResponse: any[]): Promise<void> {
     const userIds: string[] = this._getUserIdsFromComments(commentsResponse);
-
     const usersInfo = await this.resolve(userIds);
     const convert = (usersData: any[]): UserMentionDto => {
       const replacement = {};
@@ -78,16 +77,16 @@ export class MentionService {
       return replacement;
     };
     for (const comment of commentsResponse) {
-      if (comment?.parent?.mentions.length) {
+      if (comment?.parent) {
         comment.parent.mentions = convert(
           comment.parent.mentions.map((userId) => usersInfo.find((u) => u.id === userId))
         );
       }
-      if (comment.mentions && comment.mentions.length) {
-        comment.mentions = convert(
-          comment.mentions.map((userId) => usersInfo.find((u) => u.id === userId))
-        );
-      }
+      // if (comment.mentions && comment.mentions.length) {
+      comment.mentions = convert(
+        comment.mentions.map((userId) => usersInfo.find((u) => u.id === userId))
+      );
+      // }
       if (comment.child?.list && comment.child?.list.length) {
         for (const cm of comment.child?.list) {
           cm.mentions = convert(
@@ -109,7 +108,7 @@ export class MentionService {
       }
       if (comment.child?.list && comment.child?.list.length) {
         for (const cm of comment.child.list) {
-          if (cm?.length) userIds.push(...cm.mentions);
+          if (cm.mentions?.length) userIds.push(...cm.mentions);
         }
       }
     }
