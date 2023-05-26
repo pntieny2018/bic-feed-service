8cb6c3eddc1db6cc6abb075405a237e9ca3af221
thanhphat-pic <thanhphat@evol.vn>
2023-05-26 15:04:28 +0700

    feat: BEIN-13171 create series domain service

---
+++ apps/api/src/modules/v2-post/domain/domain-service/series.domain-service.ts
@@ -0,0 +1,63 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import {
+  IMediaDomainService,
+  MEDIA_DOMAIN_SERVICE_TOKEN,
+} from './interface/media.domain-service.interface';
+import { CreateSeriesProps, ISeriesDomainService } from './interface';
+import { SeriesEntity } from '../model/content/series.entity';
+import { CONTENT_REPOSITORY_TOKEN, IContentRepository } from '../repositoty-interface';
+import { ISeriesFactory, SERIES_FACTORY_TOKEN } from '../factory/interface';
+import { InvalidResourceImageException } from '../exception/invalid-resource-image.exception';
+import { ISeriesValidator, SERIES_VALIDATOR_TOKEN } from '../validator/interface';
+import { DatabaseException } from '../../../../common/exceptions/database.exception';
+
+@Injectable()
+export class SeriesDomainService implements ISeriesDomainService {
+  private readonly _logger = new Logger(SeriesDomainService.name);
+
+  public constructor(
+    @Inject(MEDIA_DOMAIN_SERVICE_TOKEN)
+    private readonly _mediaDomainService: IMediaDomainService,
+    @Inject(SERIES_FACTORY_TOKEN)
+    private readonly _seriesFactory: ISeriesFactory,
+    @Inject(SERIES_VALIDATOR_TOKEN)
+    private readonly _seriesValidator: ISeriesValidator,
+    @Inject(CONTENT_REPOSITORY_TOKEN)
+    private readonly _contentRepository: IContentRepository
+  ) {}
+
+  public async create(input: CreateSeriesProps): Promise<SeriesEntity> {
+    const { actor, title, summary, groupIds, coverMedia, setting, groups } = input.data;
+    const seriesEntity = this._seriesFactory.createSeries({
+      userId: actor.id,
+      title,
+      summary,
+    });
+
+    seriesEntity.setSetting(setting);
+
+    if (groupIds.length) {
+      const isEnableSetting = seriesEntity.getState().enableSetting;
+      await this._seriesValidator.checkCanCreateSeries(actor, groups, isEnableSetting);
+      seriesEntity.setGroups(groupIds);
+      seriesEntity.setPrivacyFromGroups(groups);
+    }
+
+    if (coverMedia && coverMedia?.id) {
+      const image = await this._mediaDomainService.getImage(
+        coverMedia.id,
+        seriesEntity.get('createdBy')
+      );
+      if (image && !image.isSeriesCoverResource()) throw new InvalidResourceImageException();
+      seriesEntity.setCover(image);
+    }
+
+    try {
+      await this._contentRepository.create(seriesEntity);
+      return seriesEntity;
+    } catch (e) {
+      this._logger.error(JSON.stringify(e?.stack));
+      throw new DatabaseException();
+    }
+  }
+}
