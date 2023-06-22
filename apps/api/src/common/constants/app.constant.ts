36c17b8facbd5d429173092dfb13aae9faa18e75
thanhphat-pic <thanhphat@evol.vn>
2023-06-22 11:43:12 +0700

    feat: BEIN-13168 pump version

--- apps/api/src/common/constants/app.constant.ts
+++ apps/api/src/common/constants/app.constant.ts
@@ -4,6 +4,7 @@ export const VERSION_1_2_2 = '1.2.3';
 export const VERSION_1_3_0 = '1.3.0';
 export const VERSION_1_4_0 = '1.4.0';
 export const VERSION_1_5_0 = '1.5.0';
+export const VERSION_1_6_0 = '1.6.0';
 export const MINIMUM_VERSION_SUPPORT = VERSION_1_2_2;
 export const VERSIONS_SUPPORTED = [VERSION_1_2_2, VERSION_1_3_0, VERSION_1_4_0, VERSION_1_5_0];
 export enum Language {
