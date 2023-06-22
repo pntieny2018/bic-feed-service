9a0248ce676df756b26898395f64a6fb8a22e786
thanhphat-pic <thanhphat@evol.vn>
2023-06-22 14:02:27 +0700

    feat: BEIN-13168 define article command

---
+++ apps/api/src/modules/v2-post/application/command/update-article/update-article.command.ts
@@ -0,0 +1,43 @@
+import { ICommand } from '@nestjs/cqrs';
+import { UserDto } from '../../../../v2-user/application/user.dto';
+import { MediaDto } from '../../../driving-apdater/dto/request';
+
+export type UpdateArticleCommandPayload = {
+  id: string;
+
+  actor: UserDto;
+
+  title?: string;
+
+  summary?: string;
+
+  content?: string;
+
+  categories?: string[];
+
+  series?: string[];
+
+  tags?: string[];
+
+  groupIds?: string[];
+
+  hashtags?: string[];
+
+  coverMedia?: MediaDto;
+
+  wordCount?: number;
+
+  mentions?: string[];
+
+  linkPreview?: {
+    url: string;
+    domain: string;
+    image: string;
+    title: string;
+    description: string;
+  };
+};
+
+export class UpdateArticleCommand implements ICommand {
+  public constructor(public readonly payload: UpdateArticleCommandPayload) {}
+}
