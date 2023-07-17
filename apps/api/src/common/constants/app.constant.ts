070f4db3911d40d01b2b022ad2ae2db4e80b4abf
thanhphat-pic <thanhphat@evol.vn>
2023-07-17 14:29:01 +0700

    feat: BEIN-16870 update app versioning

--- apps/api/src/common/constants/app.constant.ts
+++ apps/api/src/common/constants/app.constant.ts
@@ -1,20 +1,10 @@
 export const LANGUAGE_HEADER = 'x-lang';
 export const VERSION_HEADER_KEY = 'x-version-id';
-export const VERSION_1_2_2 = '1.2.3';
-export const VERSION_1_3_0 = '1.3.0';
-export const VERSION_1_4_0 = '1.4.0';
 export const VERSION_1_5_0 = '1.5.0';
 export const VERSION_1_6_0 = '1.6.0';
 export const VERSION_1_7_0 = '1.7.0';
-export const MINIMUM_VERSION_SUPPORT = VERSION_1_2_2;
-export const VERSIONS_SUPPORTED = [
-  VERSION_1_2_2,
-  VERSION_1_3_0,
-  VERSION_1_4_0,
-  VERSION_1_5_0,
-  VERSION_1_6_0,
-  VERSION_1_7_0,
-];
+export const MINIMUM_VERSION_SUPPORT = VERSION_1_5_0;
+export const VERSIONS_SUPPORTED = [VERSION_1_5_0, VERSION_1_6_0, VERSION_1_7_0];
 export enum Language {
   en = 'en',
   vn = 'vn',
