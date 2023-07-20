fcaf2bdd84b8b31bc3d732be22faaaa4d7d78af4
anhtu-bic <anhtu@evol.vn>
2023-07-20 11:31:31 +0700

    feat: BEIN-17120 unit test link-preview repository

---
+++ apps/api/src/modules/v2-post/test/driven-adapter/repository/link-preview.repository.spec.ts
@@ -0,0 +1,105 @@
+import { ILinkPreviewRepository } from '../../../domain/repositoty-interface';
+import { createMock } from '@golevelup/ts-jest';
+import { Transaction } from 'sequelize';
+import { Test, TestingModule } from '@nestjs/testing';
+import { LinkPreviewRepository } from '../../../driven-adapter/repository/link-preview.repository';
+import { getModelToken } from '@nestjs/sequelize';
+import { ILinkPreview, LinkPreviewModel } from '../../../../../database/models/link-preview.model';
+import { LinkPreviewFactory } from '../../../domain/factory/link-preview.factory';
+import { LINK_PREVIEW_FACTORY_TOKEN } from '../../../domain/factory/interface/link-preview.factory.interface';
+import { Sequelize } from 'sequelize-typescript';
+import { LinkPreviewEntity } from '../../../domain/model/link-preview';
+
+const mockLinkPreviewRecord: ILinkPreview = {
+  id: '7b63852c-5249-499a-a32b-6bdaa2761fc1',
+  url: 'string',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const mockLinkPreviewEntity = new LinkPreviewEntity(mockLinkPreviewRecord);
+
+const transaction = createMock<Transaction>();
+
+describe('LinkPreviewRepository', () => {
+  let repo: ILinkPreviewRepository;
+  let linkPreViewModel, linkPreviewFactory;
+  let sequelizeConnection;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LinkPreviewRepository,
+        {
+          provide: getModelToken(LinkPreviewModel),
+          useValue: createMock<LinkPreviewModel>(),
+        },
+        {
+          provide: LINK_PREVIEW_FACTORY_TOKEN,
+          useValue: createMock<LinkPreviewFactory>(),
+        },
+        {
+          provide: Sequelize,
+          useValue: createMock<Sequelize>(),
+        },
+      ],
+    }).compile();
+
+    repo = module.get<ILinkPreviewRepository>(LinkPreviewRepository);
+    linkPreViewModel = module.get<LinkPreviewModel>(getModelToken(LinkPreviewModel));
+    linkPreviewFactory = module.get<LinkPreviewFactory>(LINK_PREVIEW_FACTORY_TOKEN);
+    sequelizeConnection = module.get<Sequelize>(Sequelize);
+    sequelizeConnection.transaction.mockResolvedValue(transaction);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should create link preview model success', () => {
+    const spy = jest.spyOn(linkPreViewModel, 'create');
+    repo.create(mockLinkPreviewEntity);
+    expect(spy).toBeCalledWith(mockLinkPreviewRecord);
+  });
+
+  it('should update link preview model success', () => {
+    const spy = jest.spyOn(linkPreViewModel, 'update');
+    repo.update(mockLinkPreviewEntity);
+    expect(spy).toBeCalledWith(mockLinkPreviewRecord, {
+      where: {
+        id: mockLinkPreviewEntity.get('id'),
+      },
+    });
+  });
+
+  it('should find link preview model success', async () => {
+    const spy = jest.spyOn(linkPreViewModel, 'findOne').mockResolvedValue({
+      toJSON: () => mockLinkPreviewRecord,
+    });
+    const spyReconstitute = jest
+      .spyOn(linkPreviewFactory, 'reconstitute')
+      .mockResolvedValue(mockLinkPreviewEntity);
+    const result = await repo.findByUrl(mockLinkPreviewEntity.get('url'));
+
+    expect(spy).toBeCalledWith({
+      where: {
+        url: mockLinkPreviewEntity.get('url'),
+      },
+    });
+    expect(spyReconstitute).toBeCalledWith(mockLinkPreviewRecord);
+    expect(result).toEqual(mockLinkPreviewEntity);
+  });
+
+  it('should find link preview model null', async () => {
+    const spy = jest.spyOn(linkPreViewModel, 'findOne').mockResolvedValue(null);
+
+    const result = await repo.findByUrl(mockLinkPreviewEntity.get('url'));
+
+    expect(spy).toBeCalledWith({
+      where: {
+        url: mockLinkPreviewEntity.get('url'),
+      },
+    });
+    expect(result).toBeNull();
+  });
+});
