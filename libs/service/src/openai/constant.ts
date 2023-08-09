42ec7a39b5e5a144d115dc540c1547f8da755df0
anhtu-bic <anhtu@evol.vn>
2023-08-09 09:04:49 +0700

    refactor: BEIN-17467 openai libs service

---
+++ libs/service/src/openai/constant.ts
@@ -0,0 +1,5 @@
+export const TOKEN_PER_QUESTION_OR_ANSWER = 40;
+export const MAX_TOKEN = 16000;
+export const TOKEN_IN_CONTEXT = 300;
+export const CORRECT_ANSWER_KEY = '=>';
+export const MAX_COMPLETION_TOKEN = MAX_TOKEN - TOKEN_IN_CONTEXT - 400; // 400 is buffer token for content
