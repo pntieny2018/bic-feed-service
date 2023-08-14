e6e6ae77617e87443705a5ed3fdf5aace8817163
thanhphat-pic <thanhphat@evol.vn>
2023-08-14 14:32:55 +0700

    feat: BEIN-17722 update unit test for comment action

--- apps/api/src/modules/v2-post/test/mock/comment.props.mock.ts
+++ apps/api/src/modules/v2-post/test/mock/comment.props.mock.ts
@@ -4,21 +4,17 @@ import { CommentEntity } from '../../domain/model/comment';
 import { userMentions, userMock } from './user.dto.mock';
 
 export const createCommentProps: CreateCommentProps = {
-  data: {
-    userId: '6235bc91-2255-4f4b-bcfa-bebcd24e27ac',
-    postId: '9bacb01c-deae-4d80-81d7-b619b26ef684',
-    parentId: '00000000-0000-0000-0000-000000000000',
-    content: 'This is a comment',
-    giphyId: 'EZICHGrSD5QEFCxMiC',
-    media: {
-      files: [],
-      images: ['95809c73-e73e-4c49-855a-67ff0ca58346'],
-      videos: [],
-    },
-    mentions: ['0fa01fde-7c15-4d55-b60a-8e990123bc2e'],
+  userId: '6235bc91-2255-4f4b-bcfa-bebcd24e27ac',
+  postId: '9bacb01c-deae-4d80-81d7-b619b26ef684',
+  parentId: '00000000-0000-0000-0000-000000000000',
+  content: 'This is a comment',
+  giphyId: 'EZICHGrSD5QEFCxMiC',
+  media: {
+    files: [],
+    images: ['95809c73-e73e-4c49-855a-67ff0ca58346'],
+    videos: [],
   },
-  groups: [{ id: '7251dac7-5088-4a33-b900-d1b058edaf99' }].map((item) => new GroupDto(item)),
-  mentionUsers: userMentions,
+  mentions: ['0fa01fde-7c15-4d55-b60a-8e990123bc2e'],
 };
 
 export const updateCommentProps: UpdateCommentProps = {
