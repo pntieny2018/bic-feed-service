1973658c12ea035edc8f6865f6fe5856d1df993a
thanhphat-pic <thanhphat@evol.vn>
2023-08-17 11:28:58 +0700

    feat: BEIN-17730 fix eslint format issues, correct typo newsfeed

--- apps/api/src/modules/v2-post/driving-apdater/controller/newsfeed.controller.ts
+++ apps/api/src/modules/v2-post/driving-apdater/controller/newsfeed.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
-import { UserDto } from '../../../v2-user/application';
+
 import { VERSIONS_SUPPORTED } from '../../../../common/constants';
-import { NewsfeedRequestDto } from '../dto/request';
-import { FindNewsfeedQuery } from '../../application/query/find-newsfeed/find-newsfeed.query';
 import { AuthUser } from '../../../../common/decorators';
+import { UserDto } from '../../../v2-user/application';
+import { FindNewsfeedQuery } from '../../application/query/find-newsfeed/find-newsfeed.query';
+import { NewsfeedRequestDto } from '../dto/request';
 
 @ApiTags('v2 Timeline')
 @ApiSecurity('authorization')
