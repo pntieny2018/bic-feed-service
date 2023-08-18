1156ff9e8f709053d04f1e976b86092f40b367d4
Phi Dang <--global>
2023-08-18 17:25:27 +0700

    feat: BEIN-18073 random question

--- apps/api/src/modules/v2-post/domain/domain-service/quiz.domain-service.ts
+++ apps/api/src/modules/v2-post/domain/domain-service/quiz.domain-service.ts
@@ -167,7 +167,9 @@ export class QuizDomainService implements IQuizDomainService {
       throw new QuizNotFoundException();
     }
 
-    const quizEntity = await this._quizRepository.findOne(quizQuestionEntity.get('quizId'));
+    const quizEntity = await this._quizRepository.findQuizWithQuestions(
+      quizQuestionEntity.get('quizId')
+    );
 
     if (!quizEntity) {
       throw new QuizNotFoundException();
@@ -175,8 +177,11 @@ export class QuizDomainService implements IQuizDomainService {
 
     await this._quizValidator.checkCanCUDQuizInContent(quizEntity.get('contentId'), authUser);
 
+    quizEntity.deleteQuestion(id);
+
     try {
       await this._quizRepository.deleteQuestion(id);
+      await this._quizRepository.update(quizEntity);
     } catch (e) {
       this._logger.error(JSON.stringify(e?.stack));
       throw new DatabaseException();
