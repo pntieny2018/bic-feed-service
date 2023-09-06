9b2bf40deafb8e92db0dab96c96a403b9ad890b0
thanhphat-pic <thanhphat@evol.vn>
2023-09-06 14:34:47 +0700

    feat: BEIN-18608 add limit length keyword search

--- apps/api/src/modules/v2-post/driving-apdater/dto/request/search-tag.request.dto.ts
+++ apps/api/src/modules/v2-post/driving-apdater/dto/request/search-tag.request.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MinLength } from 'class-validator';
 
+const MIN_LENGTH = 3;
 export class SearchTagRequestDto {
   @ApiProperty({ description: 'Search tags by keyword', required: true })
   @IsNotEmpty()
   @Type(() => String)
+  @MinLength(MIN_LENGTH)
   @Expose({
     name: 'keyword',
   })
-  public keyword?: string;
+  public keyword: string;
 }
