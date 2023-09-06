7410b097bea8286de6dc978593b5d8a446c317ad
khanhhuyen <khanhhuyen@evol.vn>
2023-09-06 15:18:18 +0700

    refactor: BEIN-18400 add common funtion to quiz participant libary repository

--- libs/database/src/postgres/repository/interface/quiz-participant.repository.interface.ts
+++ libs/database/src/postgres/repository/interface/quiz-participant.repository.interface.ts
@@ -1,24 +1,62 @@
+import { ORDER } from '@beincom/constants';
 import {
   QuizParticipantAttributes,
   QuizParticipantModel,
 } from '@libs/database/postgres/model/quiz-participant.model';
+import { WhereOptions } from 'sequelize';
+
+import {
+  QuizParticipantAnswerAttributes,
+  QuizParticipantAnswerModel,
+} from '../../model/quiz-participant-answers.model';
+
+export type FindQuizParticipantConditionOptions = {
+  ids?: string[];
+  contentIds?: string[];
+  createdBy?: string;
+  isHighest?: boolean;
+  isFinished?: boolean;
+};
+
+export type FindQuizParticipantIncludeOptions = {
+  shouldInCludeAnswers?: boolean;
+};
+
+export type FindQuizParticipantAttributeOptions = {
+  exclude?: (keyof QuizParticipantAttributes)[];
+  include?: any[];
+};
+
+export type FindQuizParticipantOrderOptions = {
+  sortColumn?: keyof QuizParticipantAttributes;
+  sortBy?: ORDER;
+};
+
+export type FindQuizParticipantProps = {
+  condition: FindQuizParticipantConditionOptions;
+  include?: FindQuizParticipantIncludeOptions;
+  attributes?: FindQuizParticipantAttributeOptions;
+  order?: FindQuizParticipantOrderOptions;
+  group?: string[];
+};
 
 export interface ILibQuizParticipantRepository {
-  create(quizParticipant: QuizParticipantAttributes): Promise<void>;
-  update(
+  createQuizParticipant(quizParticipant: QuizParticipantAttributes): Promise<void>;
+  updateQuizParticipant(
     quizParticipantId: string,
     quizParticipant: Partial<QuizParticipantAttributes>
   ): Promise<void>;
-  findOne(takeId: string): Promise<QuizParticipantModel>;
-  findAllByContentId(contentId: string, userId: string): Promise<QuizParticipantModel[]>;
-  getQuizParticipantHighestScoreGroupByContentId(
-    contentIds: string[],
-    userId: string
-  ): Promise<Map<string, QuizParticipantModel>>;
-  getQuizParticipantsDoingGroupByContentId(
-    contentIds: string[],
-    userId: string
-  ): Promise<Map<string, QuizParticipantModel>>;
+  findQuizParticipant(findOptions: FindQuizParticipantProps): Promise<QuizParticipantModel>;
+
+  bulkCreateQuizParticipantAnswers(answers: QuizParticipantAnswerAttributes[]): Promise<void>;
+  updateQuizParticipantAnswer(
+    answerId: string,
+    answer: Partial<QuizParticipantAnswerAttributes>
+  ): Promise<void>;
+  deleteQuizParticipantAnswer(
+    conditions: WhereOptions<QuizParticipantAnswerAttributes>
+  ): Promise<void>;
+  findAllQuizParticipantAnswers(quizParticipantId: string): Promise<QuizParticipantAnswerModel[]>;
 }
 
 export const LIB_QUIZ_PARTICIPANT_REPOSITORY_TOKEN = 'LIB_QUIZ_PARTICIPANT_REPOSITORY_TOKEN';
