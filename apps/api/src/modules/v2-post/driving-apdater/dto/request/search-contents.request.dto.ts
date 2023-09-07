314655636eb9e4eed70770a4cad578bc2961c13d
thanhphat-pic <thanhphat@evol.vn>
2023-09-07 15:29:26 +0700

    feat: BEIN-18608 define swagger property name

--- apps/api/src/modules/v2-post/driving-apdater/dto/request/search-contents.request.dto.ts
+++ apps/api/src/modules/v2-post/driving-apdater/dto/request/search-contents.request.dto.ts
@@ -96,6 +96,7 @@ export class SearchContentsRequestDto {
   public groupId?: string;
 
   @ApiPropertyOptional({
+    name: 'content_types',
     description: 'Filter by type of contents',
     required: false,
   })
