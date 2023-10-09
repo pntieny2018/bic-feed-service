ca143682c641998f8724efef8e10d8fabcb8a9c0
anhtu-bic <anhtu@evol.vn>
2023-10-09 11:26:10 +0700

    feat: BEIN-19280 menu settings

--- apps/api/src/modules/v2-post/domain/domain-service/content.domain-service.ts
+++ apps/api/src/modules/v2-post/domain/domain-service/content.domain-service.ts
@@ -256,20 +256,14 @@ export class ContentDomainService implements IContentDomainService {
   }
 
   public async getContentToBuildMenuSettings(
-    id: string,
+    contentId: string,
     userId: string
   ): Promise<PostEntity | ArticleEntity | SeriesEntity> {
-    return this._contentRepository.findOne({
-      where: {
-        id,
-        groupArchived: false,
-      },
-      include: {
-        shouldIncludeGroup: true,
-        shouldIncludeQuiz: true,
-        shouldIncludeSaved: {
-          userId,
-        },
+    return this._contentRepository.findContentByIdInActiveGroup(contentId, {
+      shouldIncludeGroup: true,
+      shouldIncludeQuiz: true,
+      shouldIncludeSaved: {
+        userId,
       },
     });
   }
