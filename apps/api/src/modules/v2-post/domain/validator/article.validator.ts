8b16efb249f471e2803944a81f3754e921f57023
thanhphat-pic <thanhphat@evol.vn>
2023-10-24 13:34:29 +0700

    fix: BEIN-19789 remove unused imported library

--- apps/api/src/modules/v2-post/domain/validator/article.validator.ts
+++ apps/api/src/modules/v2-post/domain/validator/article.validator.ts
@@ -4,7 +4,6 @@ import { uniq } from 'lodash';
 
 import { ContentEmptyContentException } from '../exception';
 import { ArticleEntity } from '../model/content';
-import { CONTENT_REPOSITORY_TOKEN, IContentRepository } from '../repositoty-interface';
 import { GROUP_ADAPTER, IGroupAdapter } from '../service-adapter-interface';
 
 import { CONTENT_VALIDATOR_TOKEN, IArticleValidator, IContentValidator } from './interface';
@@ -15,9 +14,7 @@ export class ArticleValidator implements IArticleValidator {
     @Inject(GROUP_ADAPTER)
     protected _groupAdapter: IGroupAdapter,
     @Inject(CONTENT_VALIDATOR_TOKEN)
-    private readonly _contentValidator: IContentValidator,
-    @Inject(CONTENT_REPOSITORY_TOKEN)
-    private readonly _contentRepository: IContentRepository
+    private readonly _contentValidator: IContentValidator
   ) {}
 
   public async validateArticle(articleEntity: ArticleEntity, actor: UserDto): Promise<void> {
