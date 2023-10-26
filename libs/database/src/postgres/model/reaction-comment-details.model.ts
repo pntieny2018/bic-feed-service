33844760b270daaf8261ae062d91515c481074e8
anhtu-bic <anhtu@evol.vn>
2023-10-26 10:49:28 +0700

    feat: BEIN-18371 create new table reaction details

---
+++ libs/database/src/postgres/model/reaction-comment-details.model.ts
@@ -0,0 +1,39 @@
+import { IsUUID } from 'class-validator';
+import { InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+import { Column, CreatedAt, Default, PrimaryKey, Table, UpdatedAt } from 'sequelize-typescript';
+import { v4 as uuid_v4 } from 'uuid';
+
+export type ReactionCommentDetailsAttributes = InferAttributes<ReactionCommentDetailsModel>;
+
+@Table({
+  tableName: 'reaction_comment_details',
+  timestamps: false,
+})
+export class ReactionCommentDetailsModel extends Model<
+  ReactionCommentDetailsAttributes,
+  InferCreationAttributes<ReactionCommentDetailsModel>
+> {
+  @PrimaryKey
+  @IsUUID()
+  @Default(() => uuid_v4())
+  @Column
+  public id: string;
+
+  @Column
+  public reactionName: string;
+
+  @IsUUID()
+  @Column
+  public commentId: string;
+
+  @Column
+  public count: number;
+
+  @CreatedAt
+  @Column
+  public createdAt: Date;
+
+  @UpdatedAt
+  @Column
+  public updatedAt: Date;
+}
---
+++ libs/database/src/postgres/model/reaction-content-details.model.ts
@@ -0,0 +1,39 @@
+import { IsUUID } from 'class-validator';
+import { InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+import { Column, CreatedAt, Default, PrimaryKey, Table, UpdatedAt } from 'sequelize-typescript';
+import { v4 as uuid_v4 } from 'uuid';
+
+export type ReactionContentDetailsAttributes = InferAttributes<ReactionContentDetailsModel>;
+
+@Table({
+  tableName: 'reaction_content_details',
+  timestamps: false,
+})
+export class ReactionContentDetailsModel extends Model<
+  ReactionContentDetailsAttributes,
+  InferCreationAttributes<ReactionContentDetailsModel>
+> {
+  @PrimaryKey
+  @IsUUID()
+  @Default(() => uuid_v4())
+  @Column
+  public id: string;
+
+  @Column
+  public reactionName: string;
+
+  @IsUUID()
+  @Column
+  public contentId: string;
+
+  @Column
+  public count: number;
+
+  @CreatedAt
+  @Column
+  public createdAt: Date;
+
+  @UpdatedAt
+  @Column
+  public updatedAt: Date;
+}
