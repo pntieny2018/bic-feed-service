045ab84da6fe495f35a8ede94a9700b422615a9c
Phi Dang <--global>
2023-11-06 11:25:59 +0700

    refactor: BEIN-19780 remove unused code

--- apps/api/src/modules/v2-post/driving-apdater/worker-consumer/follow.consumer.ts
+++ apps/api/src/modules/v2-post/driving-apdater/worker-consumer/follow.consumer.ts
@@ -1,24 +1,35 @@
 import { Controller, Logger } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
-import { KAFKA_TOPIC } from '../../common/constants';
-import { FollowDto } from './dto/requests';
-import { FollowService } from './follow.service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { UserFollowGroupCommand } from '../../application/command/worker/user-follow-group';
+import { KAFKA_TOPIC } from '../../../../common/constants';
+import { UserUnfollowGroupCommand } from '../../application/command/worker/user-unfollow-group';
 
 @Controller()
 export class FollowConsumer {
   private _logger = new Logger(FollowConsumer.name);
 
-  public constructor(private _followService: FollowService) {}
+  public constructor(
+    private readonly _commandBus: CommandBus,
+    private readonly _queryBus: QueryBus
+  ) {}
 
   @EventPattern(KAFKA_TOPIC.BEIN_GROUP.USERS_FOLLOW_GROUPS)
-  public async follow(@Payload('value') payload: FollowDto): Promise<void> {
+  public async follow(
+    @Payload('value') payload: { userId: string; groupIds: string[]; verb: 'FOLLOW' | 'UNFOLLOW' }
+  ): Promise<void> {
     this._logger.debug(`[Event follow/unfollow]: ${JSON.stringify(payload)}`);
-    if (payload.verb === 'FOLLOW') {
-      await this._followService.follow(payload);
+    const { userId, groupIds, verb } = payload;
+    if (verb === 'FOLLOW') {
+      await this._commandBus.execute<UserFollowGroupCommand>(
+        new UserFollowGroupCommand({ userId, groupIds })
+      );
     }
 
-    if (payload.verb === 'UNFOLLOW') {
-      await this._followService.unfollow(payload);
+    if (verb === 'UNFOLLOW') {
+      await this._commandBus.execute<UserUnfollowGroupCommand>(
+        new UserUnfollowGroupCommand({ userId, groupIds })
+      );
     }
   }
 }
