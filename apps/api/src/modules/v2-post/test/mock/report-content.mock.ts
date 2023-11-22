2dfab07e00e47735fc7b95edd3bacc1501762c68
khanhhuyen <khanhhuyen@evol.vn>
2023-11-22 08:30:53 +0700

    refactor: BEIN-18401 add unit test for report mapper

--- apps/api/src/modules/v2-post/test/mock/report-content.mock.ts
+++ apps/api/src/modules/v2-post/test/mock/report-content.mock.ts
@@ -1,11 +1,33 @@
 import { CONTENT_REPORT_REASON_TYPE, CONTENT_TARGET } from '@beincom/constants';
+import { REPORT_STATUS, ReportContentAttribute } from '@libs/database/postgres/model';
 import {
   ReportContentDetailAttributes,
   REPORT_SCOPE,
 } from '@libs/database/postgres/model/report-content-detail.model';
 import { v4 } from 'uuid';
 
-export function createMockReportContentDetailRecord(
+import { ReportEntity } from '../../domain/model/report';
+
+export function createMockReportRecord(
+  data: Partial<ReportContentAttribute> = {}
+): ReportContentAttribute {
+  const id = v4();
+  const targetId = v4();
+  return {
+    id,
+    targetId,
+    targetType: CONTENT_TARGET.POST,
+    authorId: v4(),
+    status: REPORT_STATUS.CREATED,
+    updatedBy: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    details: [createMockReportDetailRecord({ id, targetId })],
+    ...data,
+  };
+}
+
+export function createMockReportDetailRecord(
   data: Partial<ReportContentDetailAttributes> = {}
 ): ReportContentDetailAttributes {
   return {
@@ -14,7 +36,7 @@ export function createMockReportContentDetailRecord(
     targetType: CONTENT_TARGET.POST,
     groupId: v4(),
     createdBy: v4(),
-    reportTo: REPORT_SCOPE.GROUP,
+    reportTo: REPORT_SCOPE.COMMUNITY,
     reportId: v4(),
     reasonType: CONTENT_REPORT_REASON_TYPE.SPAM,
     reason: 'This is spam',
@@ -22,4 +44,10 @@ export function createMockReportContentDetailRecord(
     updatedAt: new Date(),
     ...data,
   };
+}
+
+export function createMockReportEntity(data: Partial<ReportContentAttribute> = {}): ReportEntity {
+  const report = createMockReportRecord(data);
+  const { authorId: targetActorId, ...restReport } = report;
+  return new ReportEntity({ targetActorId, ...restReport });
 }
