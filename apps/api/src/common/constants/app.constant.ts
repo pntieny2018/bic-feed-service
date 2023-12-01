0823cf80f53376b284863ad3fe1e575f500d59ff
anhtu-bic <anhtu@evol.vn>
2023-12-01 10:24:05 +0700

    pump version to 1.12.0

--- apps/api/src/common/constants/app.constant.ts
+++ apps/api/src/common/constants/app.constant.ts
@@ -6,6 +6,7 @@ export const VERSION_1_8_0 = '1.8.0';
 export const VERSION_1_9_0 = '1.9.0';
 export const VERSION_1_10_0 = '1.10.0';
 export const VERSION_1_11_0 = '1.11.0';
+export const VERSION_1_12_0 = '1.12.0';
 export const MINIMUM_VERSION_SUPPORT = VERSION_1_5_0;
 export const VERSIONS_SUPPORTED = [
   VERSION_1_5_0,
@@ -15,6 +16,7 @@ export const VERSIONS_SUPPORTED = [
   VERSION_1_9_0,
   VERSION_1_10_0,
   VERSION_1_11_0,
+  VERSION_1_12_0,
 ];
 
 export const CRON_RUN_SCHEDULED_CONTENT = '0,30 * * * *';
