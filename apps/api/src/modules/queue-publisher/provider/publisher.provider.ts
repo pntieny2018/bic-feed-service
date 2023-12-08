a7ae9de43bf1b84ad282b26e235cd572df7731ed
thanhphat-pic <thanhphat@evol.vn>
2023-12-08 16:22:06 +0700

    refactor: BEIN-20962 initialize instances before starting module

--- apps/api/src/modules/queue-publisher/provider/publisher.provider.ts
+++ apps/api/src/modules/queue-publisher/provider/publisher.provider.ts
@@ -1,15 +1,3 @@
-import {
-  ContentScheduledPublisher,
-  QuizPendingPublisher,
-  QuizParticipantPublisher,
-} from '../driven-adapter/infra';
-
 export const CONTENT_SCHEDULED_PUBLISHER_TOKEN = 'CONTENT_SCHEDULED_PUBLISHER_TOKEN';
 export const QUIZ_PENDING_PUBLISHER_TOKEN = 'QUIZ_PENDING_PUBLISHER_TOKEN';
 export const QUIZ_PARTICIPANT_PUBLISHER_TOKEN = 'QUIZ_PARTICIPANT_PUBLISHER_TOKEN';
-
-export const publisherProvider = [
-  ContentScheduledPublisher,
-  QuizPendingPublisher,
-  QuizParticipantPublisher,
-];
