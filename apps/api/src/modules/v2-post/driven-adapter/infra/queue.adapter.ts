da1414b758722e203e85a3dbdf2c943ffee93886
thanhphat-pic <thanhphat@evol.vn>
2023-12-08 11:24:28 +0700

    refactor: BEIN-20962 change file application service

--- apps/api/src/modules/v2-post/driven-adapter/infra/queue.adapter.ts
+++ apps/api/src/modules/v2-post/driven-adapter/infra/queue.adapter.ts
@@ -2,8 +2,8 @@ import { QueueGroup, QueueName } from '@libs/infra/v2-queue';
 import { Inject } from '@nestjs/common';
 
 import {
-  APPLICATION_PUBLISHER_SERVICE,
-  IAppPublisherService,
+  PUBLISHER_APPLICATION_SERVICE,
+  IPublisherApplicationService,
 } from '../../../queue-publisher/application/interface';
 import {
   ContentScheduledJobDto,
@@ -14,20 +14,20 @@ import { ContentScheduledJobPayload, IQueueAdapter } from '../../domain/infra-ad
 
 export class QueueAdapter implements IQueueAdapter {
   public constructor(
-    @Inject(APPLICATION_PUBLISHER_SERVICE)
-    private readonly _appPublisherService: IAppPublisherService
+    @Inject(PUBLISHER_APPLICATION_SERVICE)
+    private readonly _publisherAppService: IPublisherApplicationService
   ) {}
 
   public async hasJob(queueName: QueueName, jobId: string): Promise<boolean> {
-    return this._appPublisherService.hasJob(queueName, jobId);
+    return this._publisherAppService.hasJob(queueName, jobId);
   }
 
   public async killJob(queueName: QueueName, jobId: string): Promise<void> {
-    await this._appPublisherService.removeJob(queueName, jobId);
+    await this._publisherAppService.removeJob(queueName, jobId);
   }
 
   public async addQuizGenerateJob(quizId: string): Promise<void> {
-    await this._appPublisherService.addJob<QuizGenerateJobDto>(QueueName.QUIZ_PENDING, {
+    await this._publisherAppService.addJob<QuizGenerateJobDto>(QueueName.QUIZ_PENDING, {
       data: { quizId },
       opts: { group: { id: QueueGroup.QUIZ_PENDING_GROUP } },
     });
@@ -37,7 +37,7 @@ export class QueueAdapter implements IQueueAdapter {
     quizParticipantId: string,
     delayTime: number
   ): Promise<void> {
-    await this._appPublisherService.addJob<QuizParticipantResultJobDto>(
+    await this._publisherAppService.addJob<QuizParticipantResultJobDto>(
       QueueName.QUIZ_PARTICIPANT_RESULT,
       {
         data: { quizParticipantId },
@@ -47,7 +47,7 @@ export class QueueAdapter implements IQueueAdapter {
   }
 
   public async addContentScheduledJobs(payloads: ContentScheduledJobPayload[]): Promise<void> {
-    await this._appPublisherService.addBulkJobs<ContentScheduledJobDto>(
+    await this._publisherAppService.addBulkJobs<ContentScheduledJobDto>(
       QueueName.CONTENT_SCHEDULED,
       payloads.map(({ contentId, ownerId }) => ({
         data: { contentId, ownerId },
