208c49fe420bbb56e6cbf2f39165d9aaec252fe6
anhtu-bic <anhtu@evol.vn>
2023-12-08 14:25:09 +0700

    feat: BEIN-20830 change to body

--- apps/api/src/modules/v2-post/driving-apdater/controller/internal.controller.ts
+++ apps/api/src/modules/v2-post/driving-apdater/controller/internal.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Version } from '@nestjs/common';
+import { Body, Controller, Post, Version } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
@@ -12,14 +12,14 @@ export class InternalController {
   public constructor(private readonly _queryBus: QueryBus) {}
 
   @ApiOperation({ summary: 'Count number content per week in community' })
-  @Get(ROUTES.INTERNAL.COUNT_CONTENTS_PER_WEEK.PATH)
+  @Post(ROUTES.INTERNAL.COUNT_CONTENTS_PER_WEEK.PATH)
   @Version(ROUTES.INTERNAL.COUNT_CONTENTS_PER_WEEK.VERSIONS)
   public async countContentsPerWeek(
-    @Query() query: CountContentPerWeekRequestDto
+    @Body() data: CountContentPerWeekRequestDto
   ): Promise<Record<string, number>> {
     return this._queryBus.execute(
       new CountContentPerWeekQuery({
-        rootGroupIds: query.rootGroupIds,
+        rootGroupIds: data.rootGroupIds,
       })
     );
   }
