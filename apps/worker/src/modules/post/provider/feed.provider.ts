d3f6f938f0adfc026e4631261cd916dbdc4b7950
thanhphat-pic <thanhphat@evol.vn>
2023-12-12 09:01:23 +0700

    refactor: BEIN-20962 change imported location

--- apps/worker/src/modules/post/provider/feed.provider.ts
+++ apps/worker/src/modules/post/provider/feed.provider.ts
@@ -1,3 +1,5 @@
+import { NewsfeedDomainService } from '../domain/domain-service';
+import { NEWSFEED_DOMAIN_SERVICE_TOKEN } from '../domain/domain-service/interface';
 import {
   FOLLOW_REPOSITORY_TOKEN,
   USER_NEWSFEED_REPOSITORY_TOKEN,
@@ -5,6 +7,11 @@ import {
 import { FollowRepository, UserNewsfeedRepository } from '../driven-adapter/repository';
 
 export const feedProvider = [
+  {
+    provide: NEWSFEED_DOMAIN_SERVICE_TOKEN,
+    useClass: NewsfeedDomainService,
+  },
+
   /* Repository */
   {
     provide: FOLLOW_REPOSITORY_TOKEN,
--- apps/worker/src/modules/post/provider/post.provider.ts
+++ apps/worker/src/modules/post/provider/post.provider.ts
@@ -1,10 +1,7 @@
 import { ContentCron } from '../application/cron/content.cron';
 import { PostCronService } from '../application/cron/post.cron';
-import { ContentDomainService, NewsfeedDomainService } from '../domain/domain-service';
-import {
-  CONTENT_DOMAIN_SERVICE_TOKEN,
-  NEWSFEED_DOMAIN_SERVICE_TOKEN,
-} from '../domain/domain-service/interface';
+import { ContentDomainService } from '../domain/domain-service';
+import { CONTENT_DOMAIN_SERVICE_TOKEN } from '../domain/domain-service/interface';
 import { CONTENT_REPOSITORY_TOKEN } from '../domain/repositoty-interface';
 import { ContentRepository } from '../driven-adapter/repository';
 
@@ -16,10 +13,6 @@ export const postProvider = [
   {
     provide: CONTENT_DOMAIN_SERVICE_TOKEN,
     useClass: ContentDomainService,
-  },
-  {
-    provide: NEWSFEED_DOMAIN_SERVICE_TOKEN,
-    useClass: NewsfeedDomainService,
   },
   {
     provide: CONTENT_REPOSITORY_TOKEN,
