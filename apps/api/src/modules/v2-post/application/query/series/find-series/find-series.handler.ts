cafb33bd1369824ac85e1720bacca76149104485
Phi Dang <--global>
2023-12-12 14:11:11 +0700

    feat: BEIN-20973 send event when get series detail

--- apps/api/src/modules/v2-post/application/query/series/find-series/find-series.handler.ts
+++ apps/api/src/modules/v2-post/application/query/series/find-series/find-series.handler.ts
@@ -1,69 +1,24 @@
 import { Inject } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-
-import {
-  ContentAccessDeniedException,
-  ContentNotFoundException,
-} from '../../../../domain/exception';
-import { SeriesEntity } from '../../../../domain/model/content';
-import {
-  CONTENT_REPOSITORY_TOKEN,
-  IContentRepository,
-} from '../../../../domain/repositoty-interface';
-import { GROUP_ADAPTER, IGroupAdapter } from '../../../../domain/service-adapter-interface';
-import { IPostValidator, POST_VALIDATOR_TOKEN } from '../../../../domain/validator/interface';
-import { ContentBinding, CONTENT_BINDING_TOKEN } from '../../../binding';
+import { CONTENT_REPOSITORY_TOKEN } from '../../../../domain/repositoty-interface';
+import { CONTENT_BINDING_TOKEN, ContentBinding } from '../../../binding';
 import { SeriesDto } from '../../../dto';
 
 import { FindSeriesQuery } from './find-series.query';
+import { ISeriesDomainService } from '@api/modules/v2-post/domain/domain-service/interface';
 
 @QueryHandler(FindSeriesQuery)
 export class FindSeriesHandler implements IQueryHandler<FindSeriesQuery, SeriesDto> {
   public constructor(
-    @Inject(GROUP_ADAPTER) private readonly _groupAdapter: IGroupAdapter,
-    @Inject(CONTENT_REPOSITORY_TOKEN) private readonly _contentRepo: IContentRepository,
-    @Inject(POST_VALIDATOR_TOKEN) private readonly _postValidator: IPostValidator,
+    @Inject(CONTENT_REPOSITORY_TOKEN) private readonly _seriesDomain: ISeriesDomainService,
     @Inject(CONTENT_BINDING_TOKEN) private readonly _contentBinding: ContentBinding
   ) {}
 
   public async execute(query: FindSeriesQuery): Promise<SeriesDto> {
     const { seriesId, authUser } = query.payload;
-    const seriesEntity = await this._contentRepo.findOne({
-      where: {
-        id: seriesId,
-        groupArchived: false,
-        excludeReportedByUserId: authUser.id,
-      },
-      include: {
-        mustIncludeGroup: true,
-        shouldIncludeItems: true,
-        shouldIncludeCategory: true,
-        shouldIncludeSaved: {
-          userId: authUser?.id,
-        },
-        shouldIncludeMarkReadImportant: {
-          userId: authUser?.id,
-        },
-      },
-    });
-
-    if (
-      !seriesEntity ||
-      !(seriesEntity instanceof SeriesEntity) ||
-      (seriesEntity.isDraft() && !seriesEntity.isOwner(authUser.id)) ||
-      seriesEntity.isHidden()
-    ) {
-      throw new ContentNotFoundException();
-    }
-
-    if (!authUser && !seriesEntity.isOpen()) {
-      throw new ContentAccessDeniedException();
-    }
-    const groups = await this._groupAdapter.getGroupsByIds(seriesEntity.get('groupIds'));
-    await this._postValidator.checkCanReadContent(seriesEntity, authUser, groups);
+    const seriesEntity = await this._seriesDomain.getSeriesById(seriesId, authUser);
 
     return this._contentBinding.seriesBinding(seriesEntity, {
-      groups,
       authUser,
     });
   }
