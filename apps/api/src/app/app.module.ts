db282b1acf39eff4afc74896fec6643214adb9b2
Phi Dang <--global>
2023-12-15 09:06:32 +0700

    feat: BEIN-21078 change http to grpc

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -43,11 +43,11 @@ import { ReactionCountModule } from '../shared/reaction-count';
 import { AppController } from './app.controller';
 import { LibModule } from './lib.module';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { OpenTelemetryModule } from '@libs/common/modules/opentelemetry';
 import * as process from 'process';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { Resource } from '@opentelemetry/resources';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 
 @Module({
   imports: [
@@ -98,7 +98,7 @@ import { Resource } from '@opentelemetry/resources';
     UserModule,
 
     OpenTelemetryModule.forRoot({
-      serviceName: process.env.APP_NAME,
+      serviceName: process.env.APP_NAME + '-grpc',
       resource: new Resource({
         [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.APP_ENV,
         [SemanticResourceAttributes.SERVICE_VERSION]: '1.0.1',
