ba8f80117c57ae0bca49917111eafa248f28f8b4
thanhphat-pic <thanhphat@evol.vn>
2023-12-21 10:18:58 +0700

    feat: BEIN-21432 correct comment code

--- apps/api/src/modules/v2-post/application/query/content/find-newsfeed/find-newsfeed.handler.ts
+++ apps/api/src/modules/v2-post/application/query/content/find-newsfeed/find-newsfeed.handler.ts
@@ -1,4 +1,3 @@
-import { FlowName } from '@libs/infra/v2-queue';
 import { Inject } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 
@@ -6,7 +5,6 @@ import {
   CONTENT_DOMAIN_SERVICE_TOKEN,
   IContentDomainService,
 } from '../../../../domain/domain-service/interface';
-import { IQueueAdapter, QUEUE_ADAPTER } from '../../../../domain/infra-adapter-interface';
 import { ContentBinding, CONTENT_BINDING_TOKEN } from '../../../binding';
 import { FindNewsfeedDto } from '../../../dto';
 
@@ -18,33 +16,25 @@ export class FindNewsfeedHandler implements IQueryHandler<FindNewsfeedQuery, Fin
     @Inject(CONTENT_BINDING_TOKEN)
     private readonly _contentBinding: ContentBinding,
     @Inject(CONTENT_DOMAIN_SERVICE_TOKEN)
-    private readonly _contentDomainService: IContentDomainService,
-    @Inject(QUEUE_ADAPTER)
-    private readonly _queueAdapter: IQueueAdapter
+    private readonly _contentDomainService: IContentDomainService
   ) {}
 
   public async execute(query: FindNewsfeedQuery): Promise<any> {
-    await this._queueAdapter.addPublishRemoveContentToNewsfeedJob({
-      contentId: '1',
-      newGroupIds: [],
-      oldGroupIds: [],
-      limit: 1000,
+    const payload = query.payload;
+    const authUserId = payload.authUser.id;
+    const { rows: ids, meta: meta } = await this._contentDomainService.getContentIdsInNewsFeed({
+      ...payload,
+      authUserId,
     });
-    // const payload = query.payload;
-    // const authUserId = payload.authUser.id;
-    // const { rows: ids, meta: meta } = await this._contentDomainService.getContentIdsInNewsFeed({
-    //   ...payload,
-    //   authUserId,
-    // });
 
-    // const contentEntities = await this._contentDomainService.getContentByIds({
-    //   ids,
-    //   authUserId,
-    // });
-    // const result = await this._contentBinding.contentsBinding(contentEntities, payload.authUser);
-    // return {
-    //   list: result,
-    //   meta,
-    // };
+    const contentEntities = await this._contentDomainService.getContentByIds({
+      ids,
+      authUserId,
+    });
+    const result = await this._contentBinding.contentsBinding(contentEntities, payload.authUser);
+    return {
+      list: result,
+      meta,
+    };
   }
 }
