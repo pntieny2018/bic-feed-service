f67f4beb254519416191039ae8ffb7b07a02eafb
thanhphat-pic <thanhphat@evol.vn>
2023-12-22 13:43:14 +0700

    feat: BEIN-21432 decrease timeout internal api

--- libs/infra/src/http/config/axios.config.ts
+++ libs/infra/src/http/config/axios.config.ts
@@ -4,26 +4,26 @@ export const getAxiosConfig = (): IAxiosConfig => ({
   group: {
     baseUrl: process.env.BE_GROUP_API_ENDPOINT,
     maxRedirects: 5,
-    timeout: 10000,
+    timeout: 5000,
   },
   user: {
     baseUrl: process.env.BE_USER_API_ENDPOINT,
     maxRedirects: 5,
-    timeout: 10000,
+    timeout: 5000,
   },
   upload: {
     baseUrl: process.env.UPLOAD_ENDPOINT,
     maxRedirects: 5,
-    timeout: 10000,
+    timeout: 5000,
   },
   lambda: {
     baseUrl: process.env.PRIVATE_API_GATEWAY,
     maxRedirects: 5,
-    timeout: 15000,
+    timeout: 5000,
   },
   notification: {
     baseUrl: process.env.BE_NOTIFICATION_API_ENDPOINT,
     maxRedirects: 5,
-    timeout: 10000,
+    timeout: 5000,
   },
 });
