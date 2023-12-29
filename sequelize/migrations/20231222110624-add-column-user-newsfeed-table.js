b9e7d5848a129af3d1e1d9148585e54475bc66f0
Phi Dang <--global>
2023-12-29 15:18:48 +0700

    refactor: BEIN-20422 fix case processed video

--- sequelize/migrations/20231222110624-add-column-user-newsfeed-table.js
+++ sequelize/migrations/20231222110624-add-column-user-newsfeed-table.js
@@ -78,21 +78,6 @@ module.exports = {
 
       await queryInterface.sequelize.query(
         `
-             DELETE FROM ${schemaName}.user_newsfeed t1
-              set created_by = t2.created_by, published_at = t2.published_at,
-                  type = t2.type,
-                  is_important = t2.is_important
-              FROM ${schemaName}.posts t2
-          WHERE
-              t2.id = t1.post_id;
-              `,
-        {
-          transaction: t,
-        }
-      );
-
-      await queryInterface.sequelize.query(
-        `
              update ${schemaName}.user_newsfeed t1
               set created_by = t2.created_by, published_at = t2.published_at,
                   type = t2.type,
