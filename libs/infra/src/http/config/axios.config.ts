50281428fb54f87a02ad00494116b67e08d14252
thanhphat-pic <thanhphat@evol.vn>
2024-01-03 09:13:41 +0700

    feat: BEIN-21432 set timeout to test handler

--- libs/infra/src/http/config/axios.config.ts
+++ libs/infra/src/http/config/axios.config.ts
@@ -24,7 +24,7 @@ export const getAxiosConfig = (): IAxiosConfig => ({
   lambda: {
     baseUrl: process.env.PRIVATE_API_GATEWAY,
     maxRedirects: 3,
-    timeout: 10000,
+    timeout: 5000,
   },
   notification: {
     baseUrl: process.env.BE_NOTIFICATION_API_ENDPOINT,
