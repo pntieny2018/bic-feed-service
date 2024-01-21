247ec1c83ff56cfb5d1dffff893c415c6b5f8a5b
Phi Dang <--global>
2024-01-21 11:51:26 +0700

    fix: BEIN-22160 remove log

--- apps/api/src/modules/v2-post/domain/validator/content.validator.ts
+++ apps/api/src/modules/v2-post/domain/validator/content.validator.ts
@@ -200,10 +200,7 @@ export class ContentValidator implements IContentValidator {
     if (post.isDraft() || post.isHidden()) {
       throw new ContentNoCRUDPermissionException();
     }
-    console.log('post.isOpen()=', post.isOpen());
-    console.log('post.isClosed()=', post.isClosed());
     if (post.isOpen() || post.isClosed()) {
-      console.log('1111');
       return;
     }
 
@@ -215,7 +212,6 @@ export class ContentValidator implements IContentValidator {
 
     const userJoinedGroupIds = user.groups ?? [];
     const canAccess = groupAudienceIds.some((groupId) => userJoinedGroupIds.includes(groupId));
-    console.log('canAccess=', canAccess);
     if (!canAccess) {
       if (options?.dataGroups?.length) {
         throw new ContentRequireGroupException(null, { requireGroups: options.dataGroups });
